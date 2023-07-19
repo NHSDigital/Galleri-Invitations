b93835e9d2f362c45fba256d49f39bb53bb2debc
Zain Malik <zain.malik3@nhs.net>
2023-07-19 15:42:24 +0100

    add sdrd table

--- terraform/environment/dev/resources/dynamodb.tf
+++ terraform/environment/dev/resources/dynamodb.tf
@@ -1,24 +1,182 @@
-resource "aws_dynamodb_table" "basic-dynamodb-table" {
-  name           = "GameScores"
+resource "aws_dynamodb_table" "sdrs-table" {
+  name           = "SDRS"
   billing_mode   = "PROVISIONED"
   read_capacity  = 20
   write_capacity = 20
-  hash_key       = "UserId"
-  range_key      = "GameTitle"
+  hash_key       = "nhs number"
+  range_key      = "given name"
 
   attribute {
-    name = "UserId"
-    type = "S"
-  }
-
-  attribute {
-    name = "GameTitle"
-    type = "S"
-  }
-
-  attribute {
-    name = "TopScore"
+    name = "nhs number"
     type = "N"
+  }
+
+  attribute {
+    name = "superseded by nhs number "
+    type = "N"
+  }
+
+  attribute {
+    name = "primary care provider"
+    type = "S"
+  }
+  attribute {
+    name = "primary care provider business effective from date "
+    type = "S"
+  }
+
+ attribute {
+    name = "current posting"
+    type = "S"
+  }
+
+ attribute {
+    name = "current posting business effective from date"
+    type = "S"
+  }
+  attribute {
+    name = "previous posting"
+    type = "S"
+  }
+
+  attribute {
+    name = "previous posting business effective from date"
+    type = "N"
+  }
+
+  attribute {
+    name = "name prefix"
+    type = "S"
+  }
+
+  attribute {
+    name = "given name"
+    type = "S"
+  }
+
+  attribute {
+    name = "other given name(s)"
+    type = "S"
+  }
+
+  attribute {
+    name = "family name"
+    type = "S"
+  }
+
+  attribute {
+    name = "previous family name"
+    type = "S"
+  }
+
+  attribute {
+    name = "date of birth"
+    type = "N"
+  }
+
+  attribute {
+    name = "gender"
+    type = "N"
+  }
+
+  attribute {
+    name = "address line 1"
+    type = "S"
+  }
+
+  attribute {
+    name = "address line 2"
+    type = "S"
+  }
+
+    attribute {
+    name = "address line 3"
+    type = "S"
+  }
+
+    attribute {
+    name = "address line 4"
+    type = "S"
+  }
+
+    attribute {
+    name = "address line 5"
+    type = "S"
+  }
+
+  attribute {
+    name = "postcode"
+    type = "S"
+  }
+
+  attribute {
+    name = "paf key"
+    type = "S"
+  }
+
+
+  attribute {
+    name = "usual address business effective from date"
+    type = "N"
+  }
+
+  attribute {
+    name = "reason for removal"
+    type = "S"
+  }
+
+  attribute {
+    name = "reason for removal effective from date"
+    type = "N"
+  }
+
+  attribute {
+    name = "date of death (formal)"
+    type = "N"
+  }
+
+  attribute {
+    name = "telephone number (home)"
+    type = "S"
+  }
+
+  attribute {
+    name = "telephone number (home) business effective from date"
+    type = "N"
+  }
+
+  attribute {
+    name = "telephone number (mobile)"
+    type = "S"
+  }
+
+  attribute {
+    name = "telephone number (mobile) business effective from date"
+    type = "N"
+  }
+  attribute {
+    name = "e-mail address (home)"
+    type = "S"
+  }
+
+  attribute {
+    name = "e-mail address (home) business effective from date"
+    type = "N"
+  }
+
+  attribute {
+    name = "preferred language"
+    type = "S"
+  }
+
+  attribute {
+    name = "interpreter required"
+    type = "B"
+  }
+
+  attribute {
+    name = "invalid flag"
+    type = "S"
   }
 
   ttl {
@@ -27,17 +185,17 @@ resource "aws_dynamodb_table" "basic-dynamodb-table" {
   }
 
   global_secondary_index {
-    name               = "GameTitleIndex"
-    hash_key           = "GameTitle"
-    range_key          = "TopScore"
+    name               = "NameIndex"
+    hash_key           = "e-mail address (home)"
+    range_key          = "telephone number (mobile)"
     write_capacity     = 10
     read_capacity      = 10
     projection_type    = "INCLUDE"
-    non_key_attributes = ["UserId"]
+    non_key_attributes = ["nhs number"]
   }
 
   tags = {
-    Name        = "dynamodb-table-1"
-    Environment = "production"
+    Name        = "dynamodb-table-sdrs"
+    Environment = "dev"
   }
 }
