7580f59420ce659e601e02eed61966e9bbae95fa
Zain Malik <zain.malik3@nhs.net>
2023-07-19 16:32:53 +0100

    linting

--- terraform/environment/dev/resources/dynamodb.tf
+++ terraform/environment/dev/resources/dynamodb.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "gp-practice-table" {
-  name           = "GP practice"
+  name           = "GP-practice"
   billing_mode   = "PROVISIONED"
   read_capacity  = 20
   write_capacity = 20
