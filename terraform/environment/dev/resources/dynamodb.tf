4c4d3656486e2d31dab285bf9f26ce5fbdf0a52e
Zain Malik <zain.malik3@nhs.net>
2023-07-19 16:11:45 +0100

    creating gp practice data store

--- terraform/environment/dev/resources/dynamodb.tf
+++ terraform/environment/dev/resources/dynamodb.tf
@@ -1,24 +1,29 @@
-resource "aws_dynamodb_table" "basic-dynamodb-table" {
-  name           = "GameScores"
+resource "aws_dynamodb_table" "gp-practice-table" {
+  name           = "GP practice"
   billing_mode   = "PROVISIONED"
   read_capacity  = 20
   write_capacity = 20
-  hash_key       = "UserId"
-  range_key      = "GameTitle"
+  hash_key       = "gp practice id"
+  range_key      = "gp practice name"
 
   attribute {
-    name = "UserId"
-    type = "S"
-  }
-
-  attribute {
-    name = "GameTitle"
-    type = "S"
-  }
-
-  attribute {
-    name = "TopScore"
+    name = "gp practice id"
     type = "N"
+  }
+
+  attribute {
+    name = "gp practice name"
+    type = "S"
+  }
+
+  attribute {
+    name = "address line 1"
+    type = "S"
+  }
+
+  attribute {
+    name = "postcode"
+    type = "S"
   }
 
   ttl {
@@ -26,18 +31,22 @@ resource "aws_dynamodb_table" "basic-dynamodb-table" {
     enabled        = false
   }
 
+  point_in_time_recovery {
+    enabled = true
+  }
+
   global_secondary_index {
-    name               = "GameTitleIndex"
-    hash_key           = "GameTitle"
-    range_key          = "TopScore"
+    name               = "EmailPhoneIndex"
+    hash_key           = "address line 1"
+    range_key          = "e-mail address (home)"
     write_capacity     = 10
     read_capacity      = 10
     projection_type    = "INCLUDE"
-    non_key_attributes = ["UserId"]
+    non_key_attributes = ["gp practice id"]
   }
 
   tags = {
-    Name        = "dynamodb-table-1"
-    Environment = "production"
+    Name        = "dynamodb-table-gp-practice"
+    Environment = "dev"
   }
 }
