4cd6423acf111d517560af104889a19203468bfa
Zain Malik <zain.malik3@nhs.net>
2023-07-20 17:35:52 +0100

    use aws key manager

--- terraform/environment/dev/resources/kms.tf
+++ terraform/environment/dev/resources/kms.tf
@@ -1,9 +1,5 @@
-data "aws_caller_identity" "current" {}
-
-
 resource "aws_kms_key" "my_kms_key" {
   description              = "My KMS Keys for Data Encryption"
-  customer_master_key_spec = "SYMMETRIC_DEFAULT"
   is_enabled               = true
   enable_key_rotation      = true
   deletion_window_in_days  = 7
