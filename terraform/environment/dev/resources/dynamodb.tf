f85d23a1416800241642f17fae13365e2f4070d7
Zain Malik <zain.malik3@nhs.net>
2023-07-21 14:48:02 +0100

    removing attempts to add resource based policy to dynamodb

--- terraform/environment/dev/resources/dynamodb.tf
+++ terraform/environment/dev/resources/dynamodb.tf
@@ -30,30 +30,3 @@ resource "aws_dynamodb_table" "participating-icb-table" {
   }
 }
 
-resource "aws_iam_policy" "dynamodb_policy" {
-  name        = "DynamoDBPolicy"
-  description = "Policy for allowing create, update, and delete actions on DynamoDB table"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "dynamodb:PutItem",
-          "dynamodb:UpdateItem",
-          "dynamodb:DeleteItem",
-        ]
-        Resource = aws_dynamodb_table.participating-icb-table.arn
-      }
-    ]
-  })
-}
-
-
-resource "aws_iam_policy_attachment" "dynamodb_attachment" {
-  name       = "DynamoDBPolicyAttachment"
-  policy_arn = aws_iam_policy.dynamodb_policy.arn
-  entities   = ["${env.AWS_ASSUME_ROLE}"]
-}
-
