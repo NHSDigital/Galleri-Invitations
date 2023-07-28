a7d2fa4d35346d98f54125a4056dac22830b4e84
Zain Malik <zain.malik3@nhs.net>
2023-07-28 18:39:41 +0100

    check reading function

--- scripts/pipeline/seed-data.sh
+++ scripts/pipeline/seed-data.sh
@@ -13,10 +13,32 @@ function main() {
 
   aws s3 cp s3://participating-icb/Participating_ICBs.csv ./test-data
 
-  cat ./test-data/Participating_ICBs.csv
+  while read line
+  do
+    echo "Record is : $line"
+  done < ./test-data/Participating_ICBs.csv
 
   # need to run the csv file and package data into a .json format like
   # the file.json below
+  # {
+  #     "PutRequest": {
+  #       "Item": {
+  #         "Id": {
+  #           "N": "$Id"
+  #         },
+  #         "IcbCode": {
+  #           "S": "$IcbCode"
+  #         },
+  #         "Board": {
+  #           "S": "$Board"
+  #         }
+  #       }
+  #     }
+  # }
+
+
+
+
 
   echo $PWD
   aws dynamodb batch-write-item --request-items \
