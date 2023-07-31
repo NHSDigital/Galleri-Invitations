8313bdfb83da9d80893415fbf0a52351f137a7bf
Zain Malik <zain.malik3@nhs.net>
2023-07-31 10:34:49 +0100

    unit test function

--- scripts/pipeline/generate_participating_icb_data.py
+++ scripts/pipeline/generate_participating_icb_data.py
@@ -22,34 +22,12 @@ import os
 #     }
 # }
 
-def read_file_participating_icb(file_path, table_name):
+def generate_participating_icb_json(file_path, table_name):
     with open(file_path, 'r', encoding='utf-8-sig') as file:
         csvreader = csv.reader(file)
-        output = []
-        # extract relavant information from row and format
-        # in dynamodb json
-        for row in csvreader:
-            Id = str(row[0])
-            IcbCode = str(row[1])
-            Board = str(row[2])
-            output.append(
-                {
-                    "PutRequest": {
-                        "Item": {
-                            "Id": {
-                                "N": f"{Id}"
-                            },
-                            "IcbCode": {
-                                "S": f"{IcbCode}"
-                            },
-                            "Board": {
-                                "S": f"{Board}"
-                            }
-                        }
-                    }
-                }
-            )
-    json_file = { table_name : output}
+        dynamodb_json_object = format_dynamodb_json(csvreader)
+
+    json_file = { table_name : dynamodb_json_object}
     json_object = json.dumps(json_file, indent=4)
 
     # create output json file
@@ -58,7 +36,79 @@ def read_file_participating_icb(file_path, table_name):
     with open(file_location, "w") as outfile:
         outfile.write(json_object)
 
-# read in data and generate the json output
-file_input_path = "/test-data/Participating_ICBs.csv"
-path_to_file = os.getcwd() + file_input_path
-read_file_participating_icb(path_to_file, "ParticipatingIcb")
+
+def format_dynamodb_json(csvreader):
+    output = []
+    # extract relavant information from row and format
+    # in dynamodb json
+    for row in csvreader:
+        Id = str(row[0])
+        IcbCode = str(row[1])
+        Board = str(row[2])
+        output.append(
+            {
+                "PutRequest": {
+                    "Item": {
+                        "Id": {
+                            "N": f"{Id}"
+                        },
+                        "IcbCode": {
+                            "S": f"{IcbCode}"
+                        },
+                        "Board": {
+                            "S": f"{Board}"
+                        }
+                    }
+                }
+            }
+        )
+    return output
+
+if __name__ == "__main__":
+    # read in data and generate the json output
+    file_input_path = "/test-data/Participating_ICBs.csv"
+    path_to_file = os.getcwd() + file_input_path
+    generate_participating_icb_json(path_to_file, "ParticipatingIcb")
+
+test_csv_data = [
+    [1, 'AAA', 'NHS test board'],
+    [2, 'BBB', 'NHS test board 1']
+]
+
+expected_output_data = [
+    {
+        "PutRequest": {
+            "Item": {
+                "Id": {
+                    "N": "1"
+                },
+                "IcbCode": {
+                    "S": "AAA"
+                },
+                "Board": {
+                    "S": "NHS test board"
+                }
+            }
+        }
+    },
+    {
+        "PutRequest": {
+            "Item": {
+                "Id": {
+                    "N": "2"
+                },
+                "IcbCode": {
+                    "S": "BBB"
+                },
+                "Board": {
+                    "S": "NHS test board 1"
+                }
+            }
+        }
+    }
+]
+
+
+def test_format_dynamodb_json():
+    assert format_dynamodb_json(test_csv_data) == expected_output_data
+
