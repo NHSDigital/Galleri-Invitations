f948c7deea4d9859b6f691a0767694494af17071
Zain Malik <zain.malik3@nhs.net>
2023-07-31 09:54:24 +0100

    rename file

--- scripts/pipeline/generate_participating_icb_data.py
+++ scripts/pipeline/generate_participating_icb_data.py
@@ -22,10 +22,12 @@ import os
 #     }
 # }
 
-def read_file_participating_icb(file_path):
+def read_file_participating_icb(file_path, table_name):
     with open(file_path, 'r', encoding='utf-8-sig') as file:
         csvreader = csv.reader(file)
         output = []
+        # extract relavant information from row and format
+        # in dynamodb json
         for row in csvreader:
             Id = str(row[0])
             IcbCode = str(row[1])
@@ -47,13 +49,16 @@ def read_file_participating_icb(file_path):
                     }
                 }
             )
-    json_file = { "ParticipatingIcb" : output}
+    json_file = { table_name : output}
     json_object = json.dumps(json_file, indent=4)
-    print(json_object)
-    # create json file
-    file_location = os.getcwd() + '/test-data/participating_icb.json'
+
+    # create output json file
+    output_json_path = "/test-data/participating_icb.json"
+    file_location = os.getcwd() + output_json_path
     with open(file_location, "w") as outfile:
         outfile.write(json_object)
 
-path_to_file = os.getcwd() + '/test-data/Participating_ICBs.csv'
-read_file_participating_icb(path_to_file)
+# read in data and generate the json output
+file_input_path = "/test-data/Participating_ICBs.csv"
+path_to_file = os.getcwd() + file_input_path
+read_file_participating_icb(path_to_file, "ParticipatingIcb")
