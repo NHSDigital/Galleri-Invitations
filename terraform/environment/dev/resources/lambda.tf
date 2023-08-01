7c0f22d6367e9487fed7c750ee40cf924e3bbc84
Zain Malik <zain.malik3@nhs.net>
2023-08-01 13:29:54 +0100

    fix the path from the flatten command

--- terraform/environment/dev/resources/lambda.tf
+++ terraform/environment/dev/resources/lambda.tf
@@ -60,8 +60,8 @@ resource "aws_cloudwatch_log_group" "data_filter_gridall_imd" {
 data "archive_file" "data_filter_gridall_imd_lambda" {
   type = "zip"
 
-  source_file = "${path.cwd}/lambda/dataFilterLambda"
-  output_path = "${path.cwd}/lambda/dataFilterLambda.zip"
+  source_file = "${path.module}/imdGridal/dataFilterLambda.js"
+  output_path = "${path.module}/imdGridal/dataFilterLambda.zip"
 }
 
 resource "aws_s3_object" "data_filter_gridall_imd_lambda" {
