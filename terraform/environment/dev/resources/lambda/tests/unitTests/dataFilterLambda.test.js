4b7f56a35c30e52715c35eda55b8868eef646eef
Zain Malik <zain.malik3@nhs.net>
2023-08-11 18:50:54 +0100

    unit test complete

--- terraform/environment/dev/resources/lambda/tests/unitTests/dataFilterLambda.test.js
+++ terraform/environment/dev/resources/lambda/tests/unitTests/dataFilterLambda.test.js
@@ -1,35 +1,102 @@
-import readCsvFromS3 from '../../filterData/lambdaHandler/dataFilterLambda.js';
-import pushCsvToS3 from '../../filterData/lambdaHandler/dataFilterLambda.js';
-import processGridallRow from '../../filterData/lambdaHandler/dataFilterLambda.js';
-import processImdRow from '../../filterData/lambdaHandler/dataFilterLambda.js';
-import generateCsvString from '../../filterData/lambdaHandler/dataFilterLambda.js';
+import { mockClient } from 'aws-sdk-client-mock';
+import { S3Client } from '@aws-sdk/client-s3';
+import { sdkStreamMixin } from '@aws-sdk/util-stream-node';
 
-import parseCsvToArray from '../../filterData/lambdaHandler/dataFilterLambda.js';
+import * as fs from 'fs';
+import path from 'path';
 
-// const testCsvString = `"PCD2","PCDS","DOINTR","DOTERM"
-// "AB1  0AA","AB1 0AA","198001","199606",
-// "YZ1  0GH","YZ1 0GH","222111","555444"`
-// const mockProcessFunction = jest.fn( () => {})
+import {
+  readCsvFromS3,
+  pushCsvToS3,
+  processGridallRow,
+  processImdRow,
+  generateCsvString,
+  parseCsvToArray
+} from '../../filterData/lambdaHandler/dataFilterLambda.js';
 
-// describe("readCsvFromS3", () => {
-//   test("resolves dataArray", () => {
+describe("readCsvFromS3", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+  test("return string built from csv file", async () => {
+    const mockS3Client = mockClient(new S3Client({}));
+    const stream = sdkStreamMixin(fs.createReadStream(path.resolve(__dirname,'./chunk_1.csv')))
 
-//   });
+    mockS3Client.resolves({
+      Body: stream,
+    });
 
-//   test("reject promise on error", () => {
+    const result = await readCsvFromS3('aaaaaaa', 'aaaaaaa', mockS3Client)
 
-//   });
-// });
+    const expected_result = '"PCD2","PCDS","DOINTR","DOTERM"\n'
 
-// describe("pushCsvToS3", () => {
-//   test("resolves dataArray", () => {
+    expect(result).toEqual(
+      expected_result
+    )
+  });
 
-//   });
+  test("Failed response when error occurs getting file to bucket", async () => {
+    const logSpy = jest.spyOn(global.console, 'log');
+    const errorMsg = new Error('Mocked error')
+    const mockClient = {
+      send: jest.fn().mockRejectedValue(errorMsg),
+    };
 
-//   test("reject promise on error", () => {
+    try {
+      await readCsvFromS3('aaaaaaa', 'aaaaaaa', mockClient);
+    } catch (err) {
+      expect(err.message).toBe('Mocked error');
+    }
 
-//   });
-// });
+    expect(logSpy).toHaveBeenCalled();
+    expect(logSpy).toHaveBeenCalledTimes(1);
+    expect(logSpy).toHaveBeenCalledWith('Failed: ', errorMsg);
+
+  });
+});
+
+describe("pushCsvToS3", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("Successful response from sending file to bucket", async () => {
+    const logSpy = jest.spyOn(global.console, 'log');
+    const mockS3Client = mockClient(new S3Client({}));
+    mockS3Client.resolves({
+      '$metadata' :
+        {'httpStatusCode': 200}
+    });
+    const result = await pushCsvToS3('galleri-ons-data','test.txt','dfsdfd',mockS3Client)
+
+    expect(logSpy).toHaveBeenCalled();
+    expect(logSpy).toHaveBeenCalledTimes(1);
+    expect(logSpy).toHaveBeenCalledWith('Succeeded');
+
+    expect(result).toHaveProperty(
+      '$metadata.httpStatusCode', 200
+    )
+  });
+
+  test("Failed response when error occurs sending file to bucket", async () => {
+    const logSpy = jest.spyOn(global.console, 'log');
+    const errorMsg = new Error('Mocked error')
+    const mockClient = {
+      send: jest.fn().mockRejectedValue(errorMsg),
+    };
+
+    try {
+      await pushCsvToS3('galleri-ons-data','test.txt','dfsdfd', mockClient);
+    } catch (err) {
+      expect(err.message).toBe('Mocked error');
+    }
+
+    expect(logSpy).toHaveBeenCalled();
+    expect(logSpy).toHaveBeenCalledTimes(1);
+    expect(logSpy).toHaveBeenCalledWith('Failed: ', errorMsg);
+
+  });
+});
 
 describe('parseCsvToArray', () => {
   const testCsvString = `"PCD2","PCDS","DOINTR","DOTERM"\n"AB1  0AA","AB1 0AA","198001","199606"\n"YZ1  0GH","YZ1 0GH","222111","555444"`
@@ -57,28 +124,28 @@ describe('parseCsvToArray', () => {
 });
 
 describe("processGridallRow", () => {
-  const row = {
-    "DOTERM" : "",
-    "CTRY" : "E92000001",
-    "PCD2": "abc  123",
-    "PCDS": "abc 123",
-    "ODSLAUA": "aaa",
-    "NHSER": "123",
-    "SICB": "bbb",
-    "CANREG": "456",
-    "OSEAST1M": "ccc",
-    "OSNRTH1M": "789",
-    "LSOA11": "ddd",
-    "MSOA11": "012",
-    "CALNCV": "eee",
-    "ICB": "QE1",
-    "OA21": "fff",
-    "LSOA21": "567",
-    "MSOA21": "ggg",
-
-  }
-  let participating_counter = 0
   test("returns participant count non-zero with correct data", () => {
+    const row = {
+      "DOTERM" : "",
+      "CTRY" : "E92000001",
+      "PCD2": "abc  123",
+      "PCDS": "abc 123",
+      "ODSLAUA": "aaa",
+      "NHSER": "123",
+      "SICB": "bbb",
+      "CANREG": "456",
+      "OSEAST1M": "ccc",
+      "OSNRTH1M": "789",
+      "LSOA11": "ddd",
+      "MSOA11": "012",
+      "CALNCV": "eee",
+      "ICB": "QE1",
+      "OA21": "fff",
+      "LSOA21": "567",
+      "MSOA21": "ggg",
+
+    }
+    let participating_counter = 0
     const dataArray = []
     const result = processGridallRow(dataArray, row, participating_counter);
 
@@ -106,9 +173,35 @@ describe("processGridallRow", () => {
 
   });
 
-  // test("returns participant count is zero with incorrect data", () => {
+  test("returns participant count is zero with incorrect data", () => {
+    const row = {
+      "DOTERM" : "not empty",
+      "CTRY" : "000",
+      "PCD2": "abc  123",
+      "PCDS": "abc 123",
+      "ODSLAUA": "aaa",
+      "NHSER": "123",
+      "SICB": "bbb",
+      "CANREG": "456",
+      "OSEAST1M": "ccc",
+      "OSNRTH1M": "789",
+      "LSOA11": "ddd",
+      "MSOA11": "012",
+      "CALNCV": "eee",
+      "ICB": "ZZZ",
+      "OA21": "fff",
+      "LSOA21": "567",
+      "MSOA21": "ggg",
 
-  // });
+    }
+    let participating_counter = 0
+    const dataArray = []
+    const result = processGridallRow(dataArray, row, participating_counter);
+
+    expect(result).toEqual(0);
+    expect(dataArray).toEqual([]);
+
+  });
 });
 
 describe("processImdRow", () => {
@@ -122,8 +215,6 @@ describe("processImdRow", () => {
     const dataArray = []
     let participating_counter = 0
     const result = processImdRow(dataArray, row, participating_counter);
-
-    expect(participating_counter).toEqual(1);
 
     expect(dataArray).toEqual([
       {
@@ -159,7 +250,3 @@ describe("generateCsvString", () => {
     )
   });
 });
-
-
-
-
