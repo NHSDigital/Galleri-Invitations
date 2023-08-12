992d9c79f2ef3924390fc646cf9dcc9162e4eaa8
Zain Malik <zain.malik3@nhs.net>
2023-08-12 15:52:40 +0100

    remove

--- terraform/environment/dev/resources/lambda.tf
+++ terraform/environment/dev/resources/lambda.tf
@@ -36,7 +36,6 @@ resource "aws_iam_policy" "iam_policy_for_lambda" {
   name                  = "aws_iam_policy_for_terraform_aws_lambda_role"
   path                  = "/"
   description           = "AWS IAM Policy for managing aws lambda role"
-  force_detach_policies = true
   policy                = <<EOF
 {
   "Statement": [
