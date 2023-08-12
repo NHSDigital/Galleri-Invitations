2fa2a56edc4d73c5969af9ebf10674f6a756c5ca
Zain Malik <zain.malik3@nhs.net>
2023-08-12 17:55:11 +0100

    formatting changes

--- terraform/environment/dev/resources/lambda.tf
+++ terraform/environment/dev/resources/lambda.tf
@@ -33,10 +33,10 @@ resource "aws_iam_role" "data_filter_gridall_imd" {
 
 resource "aws_iam_policy" "iam_policy_for_lambda" {
 
-  name                  = "aws_iam_policy_for_terraform_aws_lambda_role"
-  path                  = "/"
-  description           = "AWS IAM Policy for managing aws lambda role"
-  policy                = <<EOF
+  name        = "aws_iam_policy_for_terraform_aws_lambda_role"
+  path        = "/"
+  description = "AWS IAM Policy for managing aws lambda role"
+  policy      = <<EOF
 {
   "Statement": [
     {
