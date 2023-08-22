8114db6230e9aff2b56ff1a68dd9dcf617d26cd5
Zain Malik <zain.malik3@nhs.net>
2023-08-22 12:21:14 +0100

    test this

--- terraform/environment/dev/resources/lambda/lsoaLoader/lambdaHandler/lsoaLoader.js
+++ terraform/environment/dev/resources/lambda/lsoaLoader/lambdaHandler/lsoaLoader.js
@@ -35,12 +35,53 @@ export const pushCsvToS3 = async (bucketName, key, body, client) => {
   }
 };
 
+export const parseCsvToArray = async (csvString) => {
+  const dataArray = [];
+  let row_counter = 0;
+
+  return new Promise((resolve, reject) => {
+    Readable.from(csvString)
+      .pipe(csv())
+      .on("data", (row) => {
+        row_counter++;
+        dataArray.push(row)
+        if (row_counter === 250000) {
+          resolve(dataArray )
+        }
+      })
+      .on("error", (err) => {
+        reject(err);
+      });
+  });
+};
+
+export const generateCsvString = (header, dataArray) => {
+  return [
+    header,
+    ...dataArray.map(element => Object.values(element).join(","))
+  ].join("\n");
+};
+
 export const handler = async () => {
   const bucketName = GALLERI_ONS_BUCKET_NAME;
+  const key = LSOA_FILE_KEY;
   const client = new S3Client({})
 
-  try {
 
+
+  try {
+    const csvString = await readCsvFromS3(bucketName, key, client);
+    const dataArray = await parseCsvToArray(csvString);
+
+    const nonProdLsoaDataString = generateCsvString(
+      `POSTCODE,POSTCODE_2,LOCAL_AUT_ORG,NHS_ENG_REGION,SUB_ICB,CANCER_REGISTRY,EASTING_1M,NORTHING_1M,LSOA_2011,MSOA_2011,CANCER_ALLIANCE,ICB,OA_2021,LSOA_2021,MSOA_2021,IMD_RANK,IMD_DECILE`,
+      dataArray
+    );
+    const dateTime = new Date(Date.now()).toISOString();
+
+    const filename = `non_prod_lsoa_data_${dateTime}`
+
+    await pushCsvToS3(bucketName, `non_prod_lsoa_data_/${filename}.csv`, nonProdLsoaDataString, client);
 
   } catch (e) {
     console.error(e)
