6eacc47368789e132aca3fada56d0120fdf717dc
mandeep sandhu <mandeep.sandhu7@nhs.net>
2023-08-25 15:57:12 +0100

    setup lambda code for clinic information.

---
+++ terraform/environment/dev/resources/lambda/clinicInformation/lambdaHandler/clinicInformationLambda.js
@@ -0,0 +1,7 @@
+import { ListObjectsCommand, GetObjectCommand, PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { Readable } from 'stream';
+import csv from 'csv-parser';
+
+export const main = async () => {
+
+}
