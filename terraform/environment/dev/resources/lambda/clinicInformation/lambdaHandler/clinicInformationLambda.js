3e6ce32d63387f41658e6dbf760dd13a53f89d3e
Zain Malik <zain.malik3@nhs.net>
2023-09-07 14:48:10 +0100

    complete lambda

--- terraform/environment/dev/resources/lambda/clinicInformation/lambdaHandler/clinicInformationLambda.js
+++ terraform/environment/dev/resources/lambda/clinicInformation/lambdaHandler/clinicInformationLambda.js
@@ -4,16 +4,22 @@ import { DynamoDBClient, GetItemCommand } from "@aws-sdk/client-dynamodb";
   Lambda to load clinic information and pass on to GPS client.
 */
 export const handler = async (event, context) => {
+  console.log(
+    "*************\n Event = " + JSON.stringify(event, null, 2) + "\n**********"
+  );
+
   const client = new DynamoDBClient({ region: "eu-west-2" });
-  const { eventClinicId, eventClinicName } = event;
+
+  const clinicId = event.queryStringParameters.clinicId;
+  const clinicName = event.queryStringParameters.clinicName;
 
   var params = {
     Key: {
       ClinicId: {
-        S: `${eventClinicId}`,
+        S: `${clinicId}`,
       },
       ClinicName: {
-        S: `${eventClinicName}`,
+        S: `${clinicName}`,
       },
     },
     TableName: "PhlebotomySite",
@@ -25,9 +31,16 @@ export const handler = async (event, context) => {
 
   if (response.hasOwnProperty("Item")) {
     responseObject.statusCode = 200;
-    responseObject.body = response.Item;
+    (responseObject.headers = {
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "OPTIONS,GET",
+    }),
+      (responseObject.isBase64Encoded = true);
+    responseObject.body = JSON.stringify(response.Item);
   } else {
     responseObject.statusCode = 404;
+    responseObject.isBase64Encoded = true;
     responseObject.body = "error";
   }
 
