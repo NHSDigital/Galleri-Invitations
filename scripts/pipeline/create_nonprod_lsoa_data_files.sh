f763f38eb18b86fc4d9796fbdee229603d828cc6
mandeep sandhu <mandeep.sandhu7@nhs.net>
2023-09-13 16:27:10 +0100

    added load nonprod lsoa script

---
+++ scripts/pipeline/create_nonprod_lsoa_data_files.sh
@@ -0,0 +1,1 @@
+python $PWD/scripts/pipeline/nonprod_lsoa_load/nonprod_lsoa_load.py
--- scripts/pipeline/seed-data.sh
+++ scripts/pipeline/seed-data.sh
@@ -9,6 +9,19 @@ set -e
 #
 # ==============================================================================
 
+# function nonprod_lsoa_load(){
+#   mkdir nonprod-lsoa-data
+
+#   aws s3 cp s3://galleri-ons-data/non_prod_lsoa_data_//non_prod_lsoa_data_2023-08-22T15:27:52.810Z.csv ./nonprod_lsoa_data
+
+#   source $PWD/scripts/pipeline/create-nonprod_lsoa_data_files.sh
+
+#   echo Succefully created nonprod lsoa data
+
+#   aws dynamodb batch-write-item --request-items \
+#           file://$PWD/nonprod-lsoa-data/nonprod_lsoa.json
+# }
+
 function main() {
   mkdir test-data
 
@@ -20,6 +33,17 @@ function main() {
 
   aws dynamodb batch-write-item --request-items \
           file://$PWD/test-data/participating_icb.json
+
+  mkdir nonprod-lsoa-data
+
+  aws s3 cp s3://galleri-ons-data/non_prod_lsoa_data_//non_prod_lsoa_data_2023-08-22T15:27:52.810Z.csv ./nonprod_lsoa_data
+
+  source $PWD/scripts/pipeline/create-nonprod_lsoa_data_files.sh
+
+  echo Succefully created nonprod lsoa data
+
+  aws dynamodb batch-write-item --request-items \
+          file://$PWD/nonprod-lsoa-data/nonprod_lsoa.json
 }
 
 # ==============================================================================
