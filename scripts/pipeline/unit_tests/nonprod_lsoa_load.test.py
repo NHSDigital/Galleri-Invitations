7f348c4c59651b07271e679638a7808688a93f4d
mandeep sandhu <mandeep.sandhu7@nhs.net>
2023-09-13 16:55:32 +0100

    wip - unit test for lsoa load

---
+++ scripts/pipeline/unit_tests/nonprod_lsoa_load.test.py
@@ -0,0 +1,44 @@
+# from scripts.pipeline.nonprod_lsoa_load.nonprod_lsoa_load import format_dynamodb_json
+
+# test_csv_data = [
+#     [1, 'AAA', 'NHS test board'],
+#     [2, 'BBB', 'NHS test board 1']
+# ]
+
+# expected_output_data = [
+#     {
+#         "PutRequest": {
+#             "Item": {
+#                 "Id": {
+#                     "N": "1"
+#                 },
+#                 "IcbCode": {
+#                     "S": "AAA"
+#                 },
+#                 "Board": {
+#                     "S": "NHS test board"
+#                 }
+#             }
+#         }
+#     },
+#     {
+#         "PutRequest": {
+#             "Item": {
+#                 "Id": {
+#                     "N": "2"
+#                 },
+#                 "IcbCode": {
+#                     "S": "BBB"
+#                 },
+#                 "Board": {
+#                     "S": "NHS test board 1"
+#                 }
+#             }
+#         }
+#     }
+# ]
+
+
+# def test_format_dynamodb_json():
+#     assert format_dynamodb_json(test_csv_data) == expected_output_data
+
