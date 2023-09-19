9e7fe5c7dabb960a701bd0dfdfd71f06875f8d37
Zain Malik <zain.malik3@nhs.net>
2023-09-19 12:00:04 +0100

    adding print out to seed-data script

--- scripts/pipeline/seed-data.sh
+++ scripts/pipeline/seed-data.sh
@@ -23,27 +23,37 @@ set -e
 # }
 
 function main() {
+  echo Initiating upload of Participating ICBs test data to database
+
   mkdir test-data
 
   aws s3 cp s3://participating-icb/Participating_ICBs.csv ./test-data
 
+  echo Succefully Downloaded CSV from S3
+
   source $PWD/scripts/pipeline/create-data-files.sh
 
-  echo Succefully created test data
+  echo Succefully formatted Participating ICBs test data
 
   aws dynamodb batch-write-item --request-items \
           file://$PWD/test-data/participating_icb.json
+
+  echo Succefully uploaded Participating ICBs test data to echo Succefully created Participating ICBs database
+
+  echo "--------------------------------------------------------------"
+
+  echo Initiating upload of LSOA subset data to database
 
   mkdir nonprod-lsoa-data
 
   aws s3 cp s3://galleri-ons-data/non_prod_lsoa_data_/non_prod_lsoa_data_2023-08-22T15:27:52.810Z.csv ./nonprod-lsoa-data
 
+  echo Succefully Downloaded CSV from S3
+
   source $PWD/scripts/pipeline/create_nonprod_lsoa_data_files.sh
 
-  echo Succefully created nonprod lsoa data
+  echo Succefully uploaded Participating ICBs test data to echo Succefully created Participating ICBs database
 
-  # aws dynamodb batch-write-item --request-items \
-  #         file://$PWD/nonprod-lsoa-data/nonprod_lsoa.json
 }
 
 # ==============================================================================
