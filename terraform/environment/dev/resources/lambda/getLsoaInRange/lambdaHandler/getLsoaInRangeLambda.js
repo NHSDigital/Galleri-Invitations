45eb248d751b141c4b043b551343d0f16e51f5f1
Zain Malik <zain.malik3@nhs.net>
2023-10-23 19:16:48 +0100

    working lambda to retrieve all LSOAs and calculate distance

--- terraform/environment/dev/resources/lambda/getLsoaInRange/lambdaHandler/getLsoaInRangeLambda.js
+++ terraform/environment/dev/resources/lambda/getLsoaInRange/lambdaHandler/getLsoaInRangeLambda.js
@@ -62,6 +62,49 @@ export const handler = async (event, context) => {
       responseObject.body = "error";
   }
 
+  const command = new ScanCommand(input);
+  const response = await client.send(command);
+
+  const records = response?.Items;
+  console.log(`Total records from dynamoDB = ${records.length}`);
+
+
+  console.log(`artificial to see performance on ${newArray.flat().length} records`)
+
+  const filterRecords = records.filter((lsoaRecord) => {
+    const distanceToSiteMiles = calculateDistance(lsoaRecord, clinicGridReference);
+    if (distanceToSiteMiles <= 100) {
+      // attach to record
+      lsoaRecord.DISTANCE_TO_SITE = {
+        N: JSON.stringify(Math.round(distanceToSiteMiles * 100) / 100)
+      };
+      return lsoaRecord;
+    }
+  });
+
+
+  console.log("filterRecords length = ", filterRecords.length);
+
+  console.log("filterRecords : ", filterRecords[10]);
+
+  let responseObject = {};
+
+  if (response.hasOwnProperty("Items")) {
+    responseObject.statusCode = 200;
+    responseObject.isBase64Encoded = true;
+    (responseObject.headers = {
+      "Access-Control-Allow-Headers":
+        "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "OPTIONS,GET",
+    }),
+      (responseObject.body = JSON.stringify(filterRecords));
+  } else {
+    responseObject.statusCode = 404;
+    responseObject.isBase64Encoded = true;
+    responseObject.body = "error";
+  }
+
   const complete = Date.now() - start;
   console.log("Lambda path completion took: ", complete / 1000);
   return responseObject;
@@ -99,59 +142,6 @@ async function getClinicEastingNorthing(postcode) {
     );
     console.error("Error when trying to retrieve postcode grid reference: ");
   }
-}
-
-async function scanLsoaTable(client, lastEvaluatedItem, tableItems) {
-  const input = {
-    ExpressionAttributeNames: {
-      "#LC": "LSOA_2011",
-      "#ET": "EASTING_1M",
-      "#NT": "NORTHING_1M",
-      "#ID": "IMD_DECILE",
-      "#FU": "FORECAST_UPTAKE",
-    },
-    ProjectionExpression: "#LC, #ET, #NT, #ID, #FU",
-    TableName: "UniqueLsoa",
-  };
-  if (Object.keys(lastEvaluatedItem).length != 0) {
-    input.ExclusiveStartKey = lastEvaluatedItem;
-  }
-
-  const command = new ScanCommand(input);
-  const response = await client.send(command);
-
-  if (response.LastEvaluatedKey) {
-    console.log("response.LastEvaluatedKey = ", response.LastEvaluatedKey)
-    if (response.$metadata.httpStatusCode) {
-      console.log("Success")
-      tableItems.push(response.Items)
-      lastEvaluatedItem = response.LastEvaluatedKey
-      await scanLsoaTable(client, lastEvaluatedItem, tableItems)
-    } else {
-      console.log("Unsuccess")
-      console.error("Response from table encountered an error")
-    }
-  } else {
-    // run last invocation
-    console.log("at last bit")
-    input.ExclusiveStartKey = lastEvaluatedItem;
-    const command = new ScanCommand(input);
-    const response = await client.send(command);
-
-    if (response.$metadata.httpStatusCode) {
-      tableItems.push(response.Items)
-      return `UniqueLsoa table scanned. Returning ${tableItems.length} records`
-    } else {
-      console.error("Something went wrong with last request")
-    }
-  }
-}
-
-async function populateLsoaArray(client) {
-  const tableItems = []
-  let lastEvaluatedItem = {}
-  await scanLsoaTable(client, lastEvaluatedItem, tableItems)
-  return tableItems.flat()
 }
 
 const calculateDistance = (lsoa, clinicGridReference) => {
