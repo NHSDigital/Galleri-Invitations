2c93c956e71d3953d02fe1cf7378ed815a346bd4
Zain Malik <zain.malik3@nhs.net>
2023-10-24 11:08:48 +0100

    triggering pipeline

--- scripts/pipeline/seed-data.sh
+++ scripts/pipeline/seed-data.sh
@@ -10,22 +10,22 @@ set -e
 # ==============================================================================
 
 function main() {
-  # echo Initiating upload of Participating ICBs test data to database
+  echo Initiating upload of Participating ICBs test data to database
 
-  # mkdir test-data
+  mkdir test-data
 
-  # aws s3 cp s3://participating-icb/Participating_ICBs.csv ./test-data
+  aws s3 cp s3://participating-icb/Participating_ICBs.csv ./test-data
 
-  # echo Succefully Downloaded CSV from S3
+  echo Succefully Downloaded CSV from S3
 
-  # source $PWD/scripts/pipeline/create-data-files.sh
+  source $PWD/scripts/pipeline/create-data-files.sh
 
-  # echo Succefully formatted Participating ICBs test data
+  echo Succefully formatted Participating ICBs test data
 
-  # aws dynamodb batch-write-item --request-items \
-  #         file://$PWD/test-data/participating_icb.json
+  aws dynamodb batch-write-item --request-items \
+          file://$PWD/test-data/participating_icb.json
 
-  # echo Succefully uploaded Participating ICBs test data to database
+  echo Succefully uploaded Participating ICBs test data to database
 
   echo "--------------------------------------------------------------"
 
@@ -39,9 +39,9 @@ function main() {
 
   echo Uploading items to Postcode database
 
-  # python $PWD/scripts/pipeline/nonprod_postcode_load/nonprod_postcode_load.py
+  python $PWD/scripts/pipeline/nonprod_postcode_load/nonprod_postcode_load.py
 
-  python $PWD/nonprod_postcode_load/nonprod_postcode_load.py
+  # python $PWD/nonprod_postcode_load/nonprod_postcode_load.py
 
   # python $PWD/nonprod_postcode_load/nonprod_postcode_load.py
 
@@ -49,9 +49,9 @@ function main() {
 
   echo "--------------------------------------------------------------"
 
-  # echo Initiating upload of LSOA subset data to database
+  echo Initiating upload of LSOA subset data to database
 
-  # mkdir nonprod-lsoa-data
+  mkdir nonprod-lsoa-data
 
   aws s3 cp s3://galleri-ons-data/lsoa_data/unique_lsoa_data.csv ./nonprod-lsoa-data
 
@@ -59,23 +59,23 @@ function main() {
 
   echo Uploading items to Lsoa database
 
-  python $PWD/scripts/pipeline/nonprod_lsoa_load/nonprod_postcode_load.py
+  python $PWD/scripts/pipeline/nonprod_lsoa_load/nonprod_lsoa_load.py
 
   # python $PWD/nonprod_lsoa_load/nonprod_lsoa_load.py
 
-  # echo Succefully uploaded Lsoa data to database
+  echo Succefully uploaded Lsoa data to database
 
-  # echo "--------------------------------------------------------------"
+  echo "--------------------------------------------------------------"
 
-  # echo Initiating upload of Phlebotomy clinic data to database
+  echo Initiating upload of Phlebotomy clinic data to database
 
-  # mkdir nonprod-phlebotomy-site-load
+  mkdir nonprod-phlebotomy-site-load
 
-  # echo Uploading items to Phlebotomy clinic database
+  echo Uploading items to Phlebotomy clinic database
 
-  # python $PWD/scripts/pipeline/nonprod_phlebotomy_site_load/nonprod_phlebotomy_site_load.py
+  python $PWD/scripts/pipeline/nonprod_phlebotomy_site_load/nonprod_phlebotomy_site_load.py
 
-  # echo Succefully uploaded Phlebotomy clinic data to database
+  echo Succefully uploaded Phlebotomy clinic data to database
 
 }
 
