6d3c4c803139fc163d4518a2f7da1a2e7c4ba56a
Zain Malik <zain.malik3@nhs.net>
2023-10-24 22:09:33 +0100

    working participants in lsao code. Waiting on population file

--- terraform/environment/dev/resources/lambda/getLsoaParticipants/lambdaHandler/getLsoaParticipantsLambda.js
+++ terraform/environment/dev/resources/lambda/getLsoaParticipants/lambdaHandler/getLsoaParticipantsLambda.js
@@ -5,11 +5,11 @@ import { DynamoDBClient, QueryCommand } from "@aws-sdk/client-dynamodb";
 */
 export const handler = async (event, context) => {
   const start = Date.now();
-  console.log(
-    "*************\n Event = " + JSON.stringify(event, null, 2) + "\n**********"
-  );
+  // console.log(
+  //   "*************\n Event = " + JSON.stringify(event, null, 2) + "\n**********"
+  // );
   // destructure event to get the payload of LSOAs from front end
-  // const lsoaList = event.body.lsoaList;
+  // const lsoaList = event.queryParameterString.lsoaList;
   // placeholder lsoaList
   const lsoaList = [{
     IMD_DECILE: { N: '9' },
@@ -17,6 +17,14 @@ export const handler = async (event, context) => {
     EASTING_1M: { N: '509665' },
     FORECAST_UPTAKE: { N: '1' },
     LSOA_2011: { S: 'E01017559' },
+    DISTANCE_TO_SITE: { N: '14.19' }
+  },
+  {
+    IMD_DECILE: { N: '9' },
+    NORTHING_1M: { N: '228441' },
+    EASTING_1M: { N: '509665' },
+    FORECAST_UPTAKE: { N: '1' },
+    LSOA_2011: { S: 'E01015555' },
     DISTANCE_TO_SITE: { N: '14.19' }
   }];
 
@@ -26,69 +34,32 @@ export const handler = async (event, context) => {
   // total count of loop can be the Eligible population, and Invited would need to be another query
   // add these variables to an array of objects containing LSOA code, Eligible pop and Invited field
   // return the array
-  let populationArray = []
 
-  const lsoaCode = "E01017559"
-  const input = {
-    "ExpressionAttributeValues": {
-      ":code": {
-        "S": `${lsoaCode}`
-      }
-    },
-    "KeyConditionExpression": "LsoaCode = :code",
-    "ProjectionExpression": "PersonId",
-    "TableName": "Population",
-    "IndexName": "LsoaCode-index"
-  };
+  const eligibleInvitedPopulation = await getPopulation(lsoaList, client)
 
-  const command = new QueryCommand(input);
-  const response = await client.send(command);
+  console.log("logging response: ", eligibleInvitedPopulation)
 
-  console.log("logging response: ", JSON.stringify(response.Items))
+  let responseObject = {};
 
-  const obj = {
-    LSOA_2011: { "S": lsoaCode },
-    ELIGIBLE_POPULATION: { "S": response.Count }
-    // INVITED_POPULATION: {"S": invitedPopulation.Count}
+  if (eligibleInvitedPopulation.length > 0) {
+    responseObject.statusCode = 200;
+    responseObject.isBase64Encoded = true;
+    (responseObject.headers = {
+      "Access-Control-Allow-Headers":
+        "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "OPTIONS,GET",
+    }),
+      (responseObject.body = JSON.stringify(eligibleInvitedPopulation));
+  } else {
+    responseObject.statusCode = 404;
+    responseObject.isBase64Encoded = true;
+    responseObject.body = "error";
   }
-  // try {
-  //   populationArray = lsoaList.map(lsoa => {
-  //     const lsoaCode = lsoa.LSOA_2011.S
-  //     const eligiblePopulation = queryEligiblePopulation(lsoaCode, client)
-  //     // const invitedPopulation = queryInvitedPopulation(lsoaCode, client)
-  //     return {
-  //       LSOA_2011: {"S": lsoaCode},
-  //       ELIGIBLE_POPULATION: {"S": eligiblePopulation.Count},
-  //       // INVITED_POPULATION: {"S": invitedPopulation.Count}
-  //     }
-  //   })
-  // } catch {
-  //   console.error("Something went wrong with the queries")
-  // }
-
-  console.log(populationArray)
-
-  // let responseObject = {};
-
-  // if (response.hasOwnProperty("Items")) {
-  //   responseObject.statusCode = 200;
-  //   responseObject.isBase64Encoded = true;
-  //   (responseObject.headers = {
-  //     "Access-Control-Allow-Headers":
-  //       "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
-  //     "Access-Control-Allow-Origin": "*",
-  //     "Access-Control-Allow-Methods": "OPTIONS,GET",
-  //   }),
-  //     (responseObject.body = JSON.stringify(populationArray));
-  // } else {
-  //   responseObject.statusCode = 404;
-  //   responseObject.isBase64Encoded = true;
-  //   responseObject.body = "error";
-  // }
 
   const complete = Date.now() - start;
   console.log("Lambda path completion took: ", complete / 1000);
-  return "";
+  return responseObject;
 };
 
 // METHODS
@@ -101,10 +72,37 @@ async function queryEligiblePopulation(lsoaCode, client) {
       }
     },
     "KeyConditionExpression": "LsoaCode = :code",
-    "ProjectionExpression": "PersonId",
-    "TableName": "Population"
+    "ProjectionExpression": "PersonId, Invited",
+    "TableName": "Population",
+    "IndexName": "LsoaCode-index"
   };
 
   const command = new QueryCommand(input);
   return await client.send(command);
+}
+
+async function getPopulation(lsoaList, client) {
+  const populationArray = []
+  await Promise.all(lsoaList.map(async (lsoa) => {
+    const lsoaCode = lsoa.LSOA_2011.S
+    const response = await queryEligiblePopulation(lsoaCode, client);
+
+    // console.log("response from request = ", JSON.stringify(response.Items))
+
+    let invitedPopulation = 0
+    response.Items.forEach((person) => {
+      if (person.Invited.BOOL) {
+        ++invitedPopulation
+      }
+    })
+
+    const obj = {
+      LSOA_2011: { "S": lsoaCode },
+      ELIGIBLE_POPULATION: { "S": response.Count },
+      INVITED_POPULATION: { "S": invitedPopulation }
+    }
+
+    populationArray.push(obj)
+  }));
+  return populationArray;
 }
