7afec828047f4be55371c875de0e7e58e984cb26
Zain Malik <zain.malik3@nhs.net>
2023-10-24 11:28:03 +0100

    reduce pipeline

--- scripts/pipeline/nonprod_postcode_load/nonprod_postcode_load.py
+++ scripts/pipeline/nonprod_postcode_load/nonprod_postcode_load.py
@@ -115,4 +115,4 @@ if __name__ == "__main__":
     file_input_path = "/nonprod-postcode-data/non_prod_lsoa_data_2023-08-22T15:27:52.810Z.csv"
 
     path_to_file = os.getcwd() + file_input_path
-    generate_nonprod_lsoa_json(path_to_file, "Lsoa")
+    generate_nonprod_lsoa_json(path_to_file, "Population")
