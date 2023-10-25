c56a1722d81bef1f371440b79059bfcab78bd3a6
Abdul-NHS <abdul.shaker1@nhs.net>
2023-10-25 20:21:37 +0100

    gal-473# wip, updated grouping func to add avgEasting and avgNorthing to csv array

--- scripts/test_data/generateLsoaMidpoint.js
+++ scripts/test_data/generateLsoaMidpoint.js
@@ -30,11 +30,39 @@ const processData = async (csvString) => {
   });
 };
 
+//need to drill down inside each object which is grouped and obtain EASTING_1M and NORTHING_1M total.
+//calculate avg i.e. [easting_1m, easting_1m, ... n]/n
+
 //func to group postcodes by lsoa, prop is the property groupBy order
 function groupBy(arr, prop) {
   const map = new Map(Array.from(arr, (obj) => [obj[prop], []]));
-  arr.forEach((obj) => map.get(obj[prop]).push(obj));
+  arr.forEach((obj) => {
+    map.get(obj[prop]).push(obj);
+  });
+
+  map.forEach((x) => {
+    let avgEasting = 0;
+    let avgNorthing = 0;
+    // console.log("x is: ");
+    // console.log(x);
+    // console.log(x.length);
+    for (let i = 0; i < x.length; i++) {
+      avgEasting += Math.floor(parseInt(x[i].EASTING_1M, 10) / x.length);
+      avgNorthing += Math.floor(parseInt(x[i].NORTHING_1M, 10) / x.length);
+    }
+    // console.log(`avgEasting: ${avgEasting}, avgNorthing: ${avgNorthing}`);
+    for (let i = 0; i < x.length; i++) {
+      x[i].AVG_EASTING = String(avgEasting);
+      x[i].AVG_NORTHING = avgNorthing.toLocaleString("en-GB", {
+        minimumIntegerDigits: 7,
+        useGrouping: false,
+      });
+      // x[i].AVG_NORTHING = String(avgNorthing);
+    }
+  });
+  // const finalArray = Array.from(map.values());
   return Array.from(map.values());
+  // console.log(finalArray);
 }
 
 //Convert string to csv
@@ -63,3 +91,6 @@ const lsoaArray = await processData(lsoaTest);
 const lsoaGrouped = groupBy(lsoaArray, "LSOA_2011");
 
 console.log(lsoaGrouped);
+
+// console.log(lsoaGrouped[0][0].POSTCODE_2);
+// console.log(typeof lsoaGrouped);
