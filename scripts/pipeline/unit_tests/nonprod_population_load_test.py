f85db180afc83a0056ef79150c5472b1fb342ba6
mandeep sandhu <mandeep.sandhu7@nhs.net>
2023-10-30 23:05:27 +0000

    Updated code to pass file format checks.

--- scripts/pipeline/unit_tests/nonprod_population_load_test.py
+++ scripts/pipeline/unit_tests/nonprod_population_load_test.py
@@ -2,7 +2,7 @@ from scripts.pipeline.nonprod_lsoa_load.nonprod_lsoa_load import format_dynamodb
 
 test_csv_data = [
     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
-     16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27]
+    16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27]
 ]
 
 expected_output_data = [
