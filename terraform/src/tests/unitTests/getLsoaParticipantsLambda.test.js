97f33aa4d0928757b4ca95adc4dceb145b0b823c
Zain Malik <zain.malik3@nhs.net>
2023-10-31 12:32:01 +0000

    completed unit tests for participants in lsoa lambda

--- terraform/src/tests/unitTests/getLsoaParticipantsLambda.test.js
+++ terraform/src/tests/unitTests/getLsoaParticipantsLambda.test.js
@@ -1,44 +1,62 @@
 import { populateEligibleArray, queryEligiblePopulation, getPopulation } from '../../getLsoaParticipants/lambdaHandler/getLsoaParticipantsLambda.js';
-
-describe('populateEligibleArray', () => {
-  const testCsvString = `"PCD2","PCDS","DOINTR","DOTERM"\n"AB1  0AA","AB1 0AA","198001","199606"\n"YZ1  0GH","YZ1 0GH","222111","555444"`
-
-  test('should parse CSV string and call processFunction for each row', async () => {
-
-    const result = await populateEligibleArray(testCsvString);
-
-    expect(result).toEqual([
-      { PCD2: 'AB1  0AA', PCDS: 'AB1 0AA', DOINTR: '198001', DOTERM: '199606' },
-      { PCD2: 'YZ1  0GH', PCDS: 'YZ1 0GH', DOINTR: '222111', DOTERM: '555444' }
-    ]);
-  });
-});
+import { mockClient } from 'aws-sdk-client-mock';
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 
 describe('queryEligiblePopulation', () => {
-  const testCsvString = `"PCD2","PCDS","DOINTR","DOTERM"\n"AB1  0AA","AB1 0AA","198001","199606"\n"YZ1  0GH","YZ1 0GH","222111","555444"`
+  const mockDynamoDbClient = mockClient(new DynamoDBClient({}));
+  const lsoaCode = "code"
+  const tableItems = []
 
-  test('should parse CSV string and call processFunction for each row', async () => {
+  test('should push items to array on successful response', async () => {
+    mockDynamoDbClient.resolves({
+      $metadata: {
+        httpStatusCode: 200
+      },
+      Body: "hello"
+    });
 
-    const result = await queryEligiblePopulation(testCsvString);
+    const result = await queryEligiblePopulation(mockDynamoDbClient, lsoaCode, tableItems);
 
-    expect(result).toEqual([
-      { PCD2: 'AB1  0AA', PCDS: 'AB1 0AA', DOINTR: '198001', DOTERM: '199606' },
-      { PCD2: 'YZ1  0GH', PCDS: 'YZ1 0GH', DOINTR: '222111', DOTERM: '555444' }
-    ]);
+    expect(result).toEqual("Success");
+  });
+
+  test('should fail on unsuccessful response', async () => {
+    const logSpy = jest.spyOn(global.console, 'log');
+
+    mockDynamoDbClient.resolves({
+      $metadata: {
+        httpStatusCode: 400
+      },
+      Body: "hello"
+    });
+
+    await queryEligiblePopulation(mockDynamoDbClient, lsoaCode, tableItems);
+
+    expect(logSpy).toHaveBeenCalled();
+    expect(logSpy).toHaveBeenCalledTimes(1);
+    expect(logSpy).toHaveBeenCalledWith('Unsuccess');
   });
 });
 
 describe('getPopulation', () => {
-  const testCsvString = `"PCD2","PCDS","DOINTR","DOTERM"\n"AB1  0AA","AB1 0AA","198001","199606"\n"YZ1  0GH","YZ1 0GH","222111","555444"`
+  const mockDynamoDbClient = mockClient(new DynamoDBClient({}));
+  const lsoaList = ["code1", "code2"]
 
-  test('should parse CSV string and call processFunction for each row', async () => {
 
-    const result = await getPopulation(testCsvString);
+  test('should loop through and add property of LSOA with population info', async () => {
+    const logSpy = jest.spyOn(global.console, 'log');
 
-    expect(result).toEqual([
-      { PCD2: 'AB1  0AA', PCDS: 'AB1 0AA', DOINTR: '198001', DOTERM: '199606' },
-      { PCD2: 'YZ1  0GH', PCDS: 'YZ1 0GH', DOINTR: '222111', DOTERM: '555444' }
-    ]);
+    mockDynamoDbClient.resolves({
+      $metadata: {
+        httpStatusCode: 200
+      },
+      Body: "hello"
+    });
+    const result = await getPopulation(lsoaList, mockDynamoDbClient);
+
+    expect(logSpy).toHaveBeenCalled();
+    expect(logSpy).toHaveBeenCalledWith(`lsoa being queried number 2. Population object has 1`);
+
   });
 });
 
