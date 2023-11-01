a41959d9bc96f918bed09681d32d631376779b03
Abdul-NHS <abdul.shaker1@nhs.net>
2023-11-01 11:33:22 +0000

    gal-473# updated unit tests

--- scripts/pipeline/unit_tests/nonprod_lsoa_load_test.py
+++ scripts/pipeline/unit_tests/nonprod_lsoa_load_test.py
@@ -1,63 +1,57 @@
 from scripts.pipeline.nonprod_lsoa_load.nonprod_lsoa_load import format_dynamodb_json
 
 test_csv_data = [
-    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 ]
 
 expected_output_data = [
     {
         "Put": {
             "Item": {
-                        'POSTCODE': {
+                        'LOCAL_AUT_ORG': {
                             'S': '1'
                         },
-                        'POSTCODE_2': {
+                        'NHS_ENG_REGION': {
                             'S': '2'
                         },
-                        'LOCAL_AUT_ORG': {
+                        'SUB_ICB': {
                             'S': '3'
                         },
-                        'NHS_ENG_REGION': {
+                        'CANCER_REGISTRY': {
                             'S': '4'
                         },
-                        'SUB_ICB': {
+                        'LSOA_2011': {
                             'S': '5'
                         },
-                        'CANCER_REGISTRY': {
+                        'MSOA_2011': {
                             'S': '6'
                         },
-                        'EASTING_1M': {
-                            'N': '7'
-                        },
-                        'NORTHING_1M': {
-                            'N': '8'
-                        },
-                        'LSOA_2011': {
-                            'S': '9'
-                        },
-                        'MSOA_2011': {
-                            'S': '10'
-                        },
                         'CANCER_ALLIANCE': {
-                            'S': '11'
+                            'S': '7'
                         },
                         'ICB': {
-                            'S': '12'
+                            'S': '8'
                         },
                         'OA_2021': {
-                            'S': '13'
+                            'S': '9'
                         },
                         'LSOA_2021': {
-                            'S': '14'
+                            'S': '10'
                         },
                         'MSOA_2021': {
-                            'S': '15'
+                            'S': '11'
                         },
                         'IMD_RANK': {
-                            'N': '16'
+                            'N': '12'
                         },
                         'IMD_DECILE': {
-                            'N': '17'
+                            'N': '13'
+                        },
+                        'AVG_EASTING': {
+                            'S': '14'
+                        }
+                        'AVG_NORTHING': {
+                            'S': '15'
                         }
             },
             'TableName': 'Table',
