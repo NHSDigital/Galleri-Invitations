23ae673482433b45997051ad4c61350196657786
Zain Malik <zain.malik3@nhs.net>
2023-11-03 15:01:53 +0000

    indexing issue

--- scripts/pipeline/unit_tests/test_nonprod_unique_lsoa_load.py
+++ scripts/pipeline/unit_tests/test_nonprod_unique_lsoa_load.py
@@ -1,7 +1,7 @@
 from scripts.pipeline.nonprod_unique_lsoa_load.nonprod_unique_lsoa_load import format_dynamodb_json
 
 test_csv_data = [
-    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 ]
 
 expected_output_data = [
