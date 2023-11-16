9124e28b27ba07d8bded77b1c6c8a0e8e0ee2a16
Andrew Cleveland <andrew.cleveland1@nhs.net>
2023-11-16 15:20:31 +0000

    AC - got elastic beanstalk working

--- terraform/modules/elastic_beanstalk/main.tf
+++ terraform/modules/elastic_beanstalk/main.tf
@@ -1,9 +1,49 @@
+locals {
+  source_files = fileset("/home/acleveland/repo/Galleri-Frontend", "**/*")
+  source_hash  = sha256(join("", [for f in local.source_files : filesha256("/home/acleveland/repo/Galleri-Frontend/${f}")]))
+}
+
 data "archive_file" "screens" {
   type = "zip"
 
-  source_dir  = var.frontend_repo_location
+  # source_dir  = var.frontend_repo_location
+  source_dir = "/home/acleveland/repo/Galleri-Frontend"
+
   output_path = "${path.cwd}/src/${var.name}.zip"
 }
+
+resource "aws_iam_role" "screens" {
+  name = "${var.environment}-${var.name}-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+      },
+    ],
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "beanstalk" {
+  role       = aws_iam_role.screens.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess-AWSElasticBeanstalk"
+}
+
+resource "aws_iam_role_policy_attachment" "screens" {
+  role       = aws_iam_role.screens.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkWebTier"
+}
+
+resource "aws_iam_instance_profile" "screens" {
+  name = "${var.environment}-${var.name}-instance_profile"
+  role = aws_iam_role.screens.name
+}
+
 
 resource "aws_s3_bucket" "screens" {
   bucket = "${var.environment}-${var.name}-frontend"
@@ -11,7 +51,7 @@ resource "aws_s3_bucket" "screens" {
 
 resource "aws_s3_object" "screens" {
   bucket = aws_s3_bucket.screens.id
-  key    = "${var.name}.zip"
+  key    = "${var.name}-${local.source_hash}.zip"
   source = data.archive_file.screens.output_path
 }
 
@@ -21,23 +61,139 @@ resource "aws_elastic_beanstalk_application" "screens" {
 }
 
 resource "aws_elastic_beanstalk_application_version" "screens" {
-  name        = var.version
+  name        = "${var.environment}-${local.source_hash}"
   application = aws_elastic_beanstalk_application.screens.name
   bucket      = aws_s3_bucket.screens.bucket
   key         = aws_s3_object.screens.key
 }
 
+resource "aws_security_group" "screens" {
+  name        = "${var.environment}-${var.name}"
+  description = "Security group for Elastic Beanstalk environment"
+  vpc_id      = var.vpc_id
+
+  tags = {
+    Name = "${var.environment}-${var.name}"
+  }
+}
+
+# Allow all inbound traffic from members of the group for clustering purposes.
+resource "aws_security_group_rule" "local_ingress" {
+  security_group_id = aws_security_group.screens.id
+
+  type      = "ingress"
+  from_port = 0
+  to_port   = 0
+  protocol  = "-1"
+  self      = true
+}
+
+# Allow all outbound traffic.
+resource "aws_security_group_rule" "local_egress" {
+  security_group_id = aws_security_group.screens.id
+
+  type      = "egress"
+  from_port = 0
+  to_port   = 0
+  protocol  = "-1"
+  self      = true
+}
+
+# allow inbound http traffic
+resource "aws_vpc_security_group_ingress_rule" "http" {
+  security_group_id = aws_security_group.screens.id
+
+  from_port   = 80
+  to_port     = 80
+  ip_protocol = "tcp"
+  cidr_ipv4   = "0.0.0.0/0"
+}
+
+# allow inbound tcp 8080
+resource "aws_vpc_security_group_ingress_rule" "tcp_8080" {
+  security_group_id = aws_security_group.screens.id
+
+  from_port   = 8080
+  to_port     = 8080
+  ip_protocol = "tcp"
+  cidr_ipv4   = "0.0.0.0/0"
+}
+
+# allow inbound https
+resource "aws_vpc_security_group_ingress_rule" "https" {
+  security_group_id = aws_security_group.screens.id
+
+  from_port   = 443
+  to_port     = 443
+  ip_protocol = "tcp"
+  cidr_ipv4   = "0.0.0.0/0"
+}
+
+# allow inbound tcp 3000
+resource "aws_vpc_security_group_ingress_rule" "tcp_3000" {
+  security_group_id = aws_security_group.screens.id
+
+  from_port   = 3000
+  to_port     = 3000
+  ip_protocol = "tcp"
+  cidr_ipv4   = "0.0.0.0/0"
+}
+
+# allow ssh on port 22
+resource "aws_vpc_security_group_ingress_rule" "ssh" {
+  security_group_id = aws_security_group.screens.id
+
+  from_port   = 22
+  to_port     = 22
+  ip_protocol = "tcp"
+  cidr_ipv4   = "10.0.0.0/16"
+}
+
 resource "aws_elastic_beanstalk_environment" "screens" {
-  name                = "${var.environment}-${var.name}"
+  name                = "test-invitations-frontend"
   application         = aws_elastic_beanstalk_application.screens.name
-  solution_stack_name = var.solution_stack_name # Change to your desired platform
+  solution_stack_name = "64bit Amazon Linux 2 v5.8.7 running Node.js 18"
   version_label       = aws_elastic_beanstalk_application_version.screens.name
 
   setting {
-    namespace = var.namespace
-    name      = var.settings_name
-    value     = var.instance_type # Change to 'LoadBalanced' if needed
+    namespace = "aws:elasticbeanstalk:environment"
+    name      = "EnvironmentType"
+    value     = "SingleInstance"
   }
 
-  # Add more settings as required
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     = aws_iam_instance_profile.screens.name
+  }
+
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "VPCId"
+    value     = var.vpc_id
+  }
+
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "Subnets"
+    value     = join(",", [var.subnet_1, var.subnet_2])
+  }
+
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "SecurityGroups"
+    value     = aws_security_group.screens.id
+  }
+
+  setting {
+    namespace = "aws:elasticbeanstalk:application:environment"
+    name      = "NPM_CONFIG_UNSAFE_PERM"
+    value     = "true"
+  }
+
+  setting {
+    namespace = "aws:elasticbeanstalk:application:environment"
+    name      = "PORT"
+    value     = 8080
+  }
 }
