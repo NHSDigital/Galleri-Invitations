cd13fb4c4476dc4da51099794be78233bd6ad3f9
Andrew Cleveland <andrew.cleveland1@nhs.net>
2023-11-22 12:23:08 +0000

    AC - updated pipeline

--- scripts/pipeline/nonprod_unique_lsoa_load/nonprod_unique_lsoa_load.py
+++ scripts/pipeline/nonprod_unique_lsoa_load/nonprod_unique_lsoa_load.py
@@ -2,12 +2,14 @@ import csv
 import os
 import boto3
 
+
 def generate_nonprod_lsoa_json(file_path, table_name):
-    with open(file_path, 'r', encoding='utf-8-sig') as file:
+    with open(file_path, "r", encoding="utf-8-sig") as file:
         csvreader = csv.reader(file)
         dynamodb_json_object = format_dynamodb_json(csvreader, table_name)
 
     batch_write_to_dynamodb(dynamodb_json_object)
+
 
 def format_dynamodb_json(csvreader, table_name):
     output = []
@@ -31,81 +33,49 @@ def format_dynamodb_json(csvreader, table_name):
         AVG_NORTHING = str(row[14])
         output.append(
             {
-                'Put': {
-                    'Item': {
-                        'LOCAL_AUT_ORG': {
-                            'S': f'{LOCAL_AUT_ORG}'
-                        },
-                        'NHS_ENG_REGION': {
-                            'S': f'{NHS_ENG_REGION}'
-                        },
-                        'SUB_ICB': {
-                            'S': f'{SUB_ICB}'
-                        },
-                        'CANCER_REGISTRY': {
-                            'S': f'{CANCER_REGISTRY}'
-                        },
-                        'LSOA_2011': {
-                            'S': f'{LSOA_2011}'
-                        },
-                        'MSOA_2011': {
-                            'S': f'{MSOA_2011}'
-                        },
-                        'CANCER_ALLIANCE': {
-                            'S': f'{CANCER_ALLIANCE}'
-                        },
-                        'ICB': {
-                            'S': f'{ICB}'
-                        },
-                        'OA_2021': {
-                            'S': f'{OA_2021}'
-                        },
-                        'LSOA_2021': {
-                            'S': f'{LSOA_2021}'
-                        },
-                        'MSOA_2021': {
-                            'S': f'{MSOA_2021}'
-                        },
-                        'IMD_RANK': {
-                            'N': f'{IMD_RANK}'
-                        },
-                        'IMD_DECILE': {
-                            'N': f'{IMD_DECILE}'
-                        },
-                        'FORECAST_UPTAKE': {
-                            'N': f'1'
-                        },
-                        'AVG_EASTING': {
-                            'S': f'{AVG_EASTING}'
-                        },
-                        'AVG_NORTHING': {
-                            'S': f'{AVG_NORTHING}'
-                        },
+                "Put": {
+                    "Item": {
+                        "LOCAL_AUT_ORG": {"S": f"{LOCAL_AUT_ORG}"},
+                        "NHS_ENG_REGION": {"S": f"{NHS_ENG_REGION}"},
+                        "SUB_ICB": {"S": f"{SUB_ICB}"},
+                        "CANCER_REGISTRY": {"S": f"{CANCER_REGISTRY}"},
+                        "LSOA_2011": {"S": f"{LSOA_2011}"},
+                        "MSOA_2011": {"S": f"{MSOA_2011}"},
+                        "CANCER_ALLIANCE": {"S": f"{CANCER_ALLIANCE}"},
+                        "ICB": {"S": f"{ICB}"},
+                        "OA_2021": {"S": f"{OA_2021}"},
+                        "LSOA_2021": {"S": f"{LSOA_2021}"},
+                        "MSOA_2021": {"S": f"{MSOA_2021}"},
+                        "IMD_RANK": {"N": f"{IMD_RANK}"},
+                        "IMD_DECILE": {"N": f"{IMD_DECILE}"},
+                        "FORECAST_UPTAKE": {"N": f"1"},
+                        "AVG_EASTING": {"S": f"{AVG_EASTING}"},
+                        "AVG_NORTHING": {"S": f"{AVG_NORTHING}"},
                     },
-                    'TableName': table_name,
+                    "TableName": table_name,
                 },
             }
         )
     return output
 
+
 def batch_write_to_dynamodb(lsoa_data):
     # splice array 100 records at a time
     # format and send these to the batch write function
     # repeat till no records left
-    dynamodb_client = boto3.client('dynamodb')
+    dynamodb_client = boto3.client("dynamodb")
     for i in range(1, len(lsoa_data), 100):
-        upper_bound_slice = i+100
+        upper_bound_slice = i + 100
         test_data = lsoa_data[i:upper_bound_slice]
-        dynamodb_client.transact_write_items(
-            TransactItems=test_data
-        )
-    return 'Finished'
+        dynamodb_client.transact_write_items(TransactItems=test_data)
+    return "Finished"
 
 
+ENVIRONMENT = os.getenv("environment")
 
 if __name__ == "__main__":
     # read in data and generate the json output
     file_input_path = "/nonprod-unique-lsoa-data/unique_lsoa_data.csv"
 
     path_to_file = os.getcwd() + file_input_path
-    generate_nonprod_lsoa_json(path_to_file, "UniqueLsoa")
+    generate_nonprod_lsoa_json(path_to_file, ENVIRONMENT + "-UniqueLsoa")
