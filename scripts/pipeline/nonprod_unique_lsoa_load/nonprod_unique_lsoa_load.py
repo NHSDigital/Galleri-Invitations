2e8173a152b54c877143c301ba1e8ee780578f5c
Andrew Cleveland <andrew.cleveland1@nhs.net>
2023-11-23 18:06:12 +0000

    AC - updated lsoa seed script

--- scripts/pipeline/nonprod_unique_lsoa_load/nonprod_unique_lsoa_load.py
+++ scripts/pipeline/nonprod_unique_lsoa_load/nonprod_unique_lsoa_load.py
@@ -68,8 +68,8 @@ def batch_write_to_dynamodb(lsoa_data):
     # format and send these to the batch write function
     # repeat till no records left
     dynamodb_client = boto3.client("dynamodb")
-    for i in range(1, len(lsoa_data), 100):
-        upper_bound_slice = i + 100
+    for i in range(1, len(lsoa_data), 50):
+        upper_bound_slice = i + 50
         test_data = lsoa_data[i:upper_bound_slice]
         print(test_data)
         dynamodb_client.transact_write_items(TransactItems=test_data)
