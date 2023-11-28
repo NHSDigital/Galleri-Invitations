ea2ac10b7171026ca30ec28fd29ddea29585af0c
Andrew Cleveland <andrew.cleveland1@nhs.net>
2023-11-28 12:05:37 +0000

    AC - updated main file to prevent loops with db updates

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,11 @@
 variable "bucket_name" {
-  type = string
+  type    = string
+  default = "galleri-lambda-bucket"
 }
-variable "role_name" {}
+variable "role_name" {
+  type    = string
+  default = "galleri-lambda-role"
+}
 
 variable "environment" {
   description = "The environment being deployed into, could be: dev, test, uat, performance or prod"
