71b6b0a9220125a0a85592e8db4a10bf2cfcf4f9
Andrew Cleveland <andrew.cleveland1@nhs.net>
2024-01-03 15:29:51 +0000

    removed junk from rebase

--- scripts/pipeline/nonprod_population_load/nonprod_population_load.py
+++ scripts/pipeline/nonprod_population_load/nonprod_population_load.py
@@ -1,9 +1,6 @@
 import csv
 import os
 import boto3
-import uuid
-import rstr
-import random
 
 ENVIRONMENT = os.getenv("environment")
 
@@ -47,8 +44,6 @@ def format_dynamodb_json(csvreader, table_name):
             sensitivity_indicator_flag = str(row[23])
             Invited = str(row[24])
             lsoa_2011 = str(row[25])
-            participant_id_rand_exp = rstr.xeger(r'NHS-[A-HJ-NP-Z][A-HJ-NP-Z][0-9][0-9]-[A-HJ-NP-Z][A-HJ-NP-Z][0-9][0-9]')
-            gp_practice_code = "GP-CODE-" + str(random.randint(1, 5000))
 
             if nhs_number and lsoa_2011:
                 output.append(
@@ -153,12 +148,6 @@ def format_dynamodb_json(csvreader, table_name):
                                 },
                                 'identified_to_be_invited': {
                                     'BOOL': False
-                                },
-                                'participantId': {
-                                    'S': f'{str(participant_id_rand_exp)}'
-                                },
-                                'gpPracticeCode': {
-                                    'S': f'{gp_practice_code}'
                                 }
                             },
                             "TableName": table_name,
