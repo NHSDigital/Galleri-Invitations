b239d9de95cd3acfd48df101351fcae2d1c3b9bb
Abdul-NHS <abdul.shaker1@nhs.net>
2024-01-04 10:36:35 +0000

    feature/GAL-833# added skeleton structure for mesh lambda

--- terraform/main.tf
+++ terraform/main.tf
@@ -606,6 +606,35 @@ module "create_episode_record_dynamodb_stream" {
   maximum_batching_window_in_seconds = 300
 }
 
+# GET daily demographic mesh lambda
+module "get_daily_demographic_mesh_lambda" {
+  source               = "./modules/lambda"
+  environment          = var.environment
+  bucket_id            = module.s3_bucket.bucket_id
+  lambda_iam_role      = module.iam_galleri_lambda_role.galleri_lambda_role_arn
+  lambda_function_name = "get_daily_demographic_mesh_lambda"
+  lambda_s3_object_key = "get_daily_demographic_mesh_lambda.zip"
+  environment_vars = {
+    ENVIRONMENT = "${var.environment}"
+  }
+}
+
+module "get_daily_demographic_mesh_lambda_cloudwatch" {
+  source               = "./modules/cloudwatch"
+  environment          = var.environment
+  lambda_function_name = module.get_daily_demographic_mesh_lambda.lambda_function_name
+  retention_days       = 14
+}
+
+module "get_daily_demographic_mesh_lambda_api_gateway" {
+  source                 = "./modules/api-gateway"
+  environment            = var.environment
+  lambda_invoke_arn      = module.get_daily_demographic_mesh_lambda.lambda_invoke_arn
+  path_part              = "daily-demographic-mesh"
+  method_http_parameters = {}
+  lambda_function_name   = module.get_daily_demographic_mesh_lambda.lambda_function_name
+}
+
 # Dynamodb tables
 module "sdrs_table" {
   source      = "./modules/dynamodb"
