8c25d33912e478457492a07c269a6cce5c7cda15
Andrew Cleveland <andrew.cleveland1@nhs.net>
2024-01-09 16:14:28 +0000

    updated tests

--- scripts/test/api-test.js
+++ scripts/test/api-test.js
@@ -26,7 +26,7 @@ const clinicInformation = {
   name: "clinic-information",
   resource_path: "clinic-information",
   method: "GET",
-  query_string: "?clinicId=WE89I939&clinicName=Phlebotomy%20clinic%208",
+  query_string: "?clinicId=AQ16C317&clinicName=Phlebotomy%20clinic%215",
   expected_status_code: 200,
   expected_response_count: 13,
 };
