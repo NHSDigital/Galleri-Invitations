c8da01b3c96af5b1e149786a26c1cc7d440209c0
Zain <zain.malik3@nhs.net>
2024-01-16 10:36:55 +0000

    accidentally commited WIP files

--- terraform/main.tf
+++ terraform/main.tf
@@ -86,6 +86,14 @@ module "caas_data_bucket" {
   environment             = var.environment
 }
 
+# Validated CaaS MESH output data bucket
+module "validate_caas_data_bucket" {
+  source                  = "./modules/s3"
+  bucket_name             = "galleri-validated-caas-data"
+  galleri_lambda_role_arn = module.iam_galleri_lambda_role.galleri_lambda_role_arn
+  environment             = var.environment
+}
+
 # Data Filter Gridall IMD
 module "data_filter_gridall_imd_lambda" {
   source               = "./modules/lambda"
@@ -689,6 +697,43 @@ module "caas_feed_add_records_lambda_cloudwatch" {
   environment          = var.environment
   lambda_function_name = module.caas_feed_add_records_lambda.lambda_function_name
   retention_days       = 14
+}
+
+module "caas_feed_add_records_lambda_trigger" {
+  source     = "./modules/lambda_trigger"
+  bucket_id  = module.validate_caas_data_bucket.bucket_id
+  bucket_arn = module.validate_caas_data_bucket.bucket_arn
+  lambda_arn = module.caas_feed_add_records_lambda.lambda_arn
+}
+
+module "validate_caas_feed_lambda" {
+  source               = "./modules/lambda"
+  environment          = var.environment
+  bucket_id            = module.s3_bucket.bucket_id
+  lambda_iam_role      = module.iam_galleri_lambda_role.galleri_lambda_role_arn
+  lambda_function_name = "validateCaasFeedLambda"
+  lambda_timeout       = 100
+  memory_size          = 1024
+  lambda_s3_object_key = "validate_caas_feed_lambda.zip"
+  environment_vars = {
+    BUCKET_NAME = "galleri-caas-data",
+    ENVIRONMENT = "${var.environment}"
+  }
+}
+
+module "validate_caas_feed_lambda_cloudwatch" {
+  source               = "./modules/cloudwatch"
+  environment          = var.environment
+  lambda_function_name = module.validate_caas_feed_lambda.lambda_function_name
+  retention_days       = 14
+}
+
+
+module "validate_caas_feed_lambda_trigger" {
+  source     = "./modules/lambda_trigger"
+  bucket_id  = module.caas_data_bucket.bucket_id
+  bucket_arn = module.caas_data_bucket.bucket_arn
+  lambda_arn = module.validate_caas_feed_lambda.lambda_arn
 }
 
 # Dynamodb tables
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,7 +13,7 @@ variable "environment" {
 
 variable "frontend_repo_location" {
   description = "The location of the Galler-Frontend repo on the filesystem"
-  default     = "/home/runner/work/Galleri-Invitations/Galleri-Invitations/Galleri-Frontend"
+  default     = "/Users/zain/projects/Galleri-Frontend"
 }
 
 # variable "aws_account_number" {}
