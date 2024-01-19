aa46ef99091041ba974fde08694a259cfb7866ff
Abdul-NHS <abdul.shaker1@nhs.net>
2024-01-19 22:00:58 +0000

    GAL-833# redirect beanstalk to galleri

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,7 +13,7 @@ variable "environment" {
 
 variable "frontend_repo_location" {
   description = "The location of the Galler-Frontend repo on the filesystem"
-  default     = "/Users/abduls/repos/Galleri-Frontend"
+  default     = "/home/runner/work/Galleri-Invitations/Galleri-Invitations/Galleri-Frontend"
 }
 
 # variable "aws_account_number" {}
