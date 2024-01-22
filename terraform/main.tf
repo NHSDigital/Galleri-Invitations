f5be01ad7d6cae4f007e126e3d888a060c2be176
Abdul-NHS <abdul.shaker1@nhs.net>
2024-01-22 12:34:57 +0000

    GAL-833# resolving merge conflict overwrites

--- terraform/main.tf
+++ terraform/main.tf
@@ -649,15 +649,15 @@ module "user_accounts_lambda_trigger" {
   lambda_arn = module.user_accounts_lambda.lambda_arn
 }
 
-module "validate_caas_feed_lambda" {
+module "poll_mesh_mailbox_lambda" {
   source               = "./modules/lambda"
   environment          = var.environment
   bucket_id            = module.s3_bucket.bucket_id
   lambda_iam_role      = module.iam_galleri_lambda_role.galleri_lambda_role_arn
-  lambda_function_name = "validateCaasFeedLambda"
+  lambda_function_name = "pollMeshMailboxLambda"
   lambda_timeout       = 100
   memory_size          = 1024
-  lambda_s3_object_key = "validate_caas_feed_lambda.zip"
+  lambda_s3_object_key = "poll_mesh_mailbox_lambda.zip"
   environment_vars = {
     ENVIRONMENT                    = "${var.environment}",
     MESH_SANDBOX                   = "false",
@@ -716,7 +716,7 @@ data "aws_secretsmanager_secret_version" "mesh_sender_cert" {
 module "poll_mesh_mailbox_lambda_cloudwatch" {
   source               = "./modules/cloudwatch"
   environment          = var.environment
-  lambda_function_name = module.validate_caas_feed_lambda.lambda_function_name
+  lambda_function_name = module.poll_mesh_mailbox_lambda.lambda_function_name
   retention_days       = 14
 }
 
