ffb5832636af866a66efb4ef93851331329ee413
Abdul-NHS <abdul.shaker1@nhs.net>
2024-01-28 11:42:18 +0000

    feature/GAL-345# creating mesh functions

--- terraform/src/gtmsMeshMailboxLambda/gtmsMeshMailboxLambda.js
+++ terraform/src/gtmsMeshMailboxLambda/gtmsMeshMailboxLambda.js
@@ -14,6 +14,19 @@ const ENVIRONMENT = process.env.ENVIRONMENT;
 const GTMS_MESH_CERT = await readSecret("GTMS_MESH_CERT", smClient);
 const MESH_GTMS_KEY = await readSecret("MESH_SENDER_KEY", smClient);
 
+const CONFIG = await loadConfig({
+  url: "https://msg.intspineservices.nhs.uk", //can leave as non-secret
+  sharedKey: process.env.MESH_SHARED_KEY,
+  sandbox: "false",
+  senderCert: GTMS_MESH_CERT,
+  senderKey: MESH_GTMS_KEY,
+  senderMailboxID: process.env.MESH_SENDER_MAILBOX_ID,
+  senderMailboxPassword: process.env.MESH_SENDER_MAILBOX_PASSWORD,
+  receiverCert: GTMS_MESH_CERT,
+  receiverKey: MESH_GTMS_KEY,
+  receiverMailboxID: process.env.MESH_RECEIVER_MAILBOX_ID,
+  receiverMailboxPassword: process.env.MESH_RECEIVER_MAILBOX_PASSWORD,
+});
 
 //HANDLER
 export const handler = async (event, context) => {
@@ -22,6 +35,76 @@ export const handler = async (event, context) => {
 
 
 //FUNCTIONS
+//Establish connection with MESH
+async function run() {
+  try {
+    let healthCheck = await handShake({
+      url: CONFIG.url,
+      mailboxID: CONFIG.senderMailboxID,
+      mailboxPassword: CONFIG.senderMailboxPassword,
+      sharedKey: CONFIG.sharedKey,
+      agent: CONFIG.senderAgent,
+    });
+
+    return healthCheck.status
+  } catch (error) {
+    console.error("Error occurred:", error);
+  }
+}
+
+//Return an array of message IDs
+async function getMessageArray() {
+  try {
+    let messageCount = await getMessageCount({
+      url: CONFIG.url,
+      mailboxID: CONFIG.senderMailboxID,
+      mailboxPassword: CONFIG.senderMailboxPassword,
+      sharedKey: CONFIG.sharedKey,
+      agent: CONFIG.senderAgent,
+    });
+    let messageList = messageCount.data.messages
+    let inboxCount = messageCount.data.approx_inbox_count;
+    console.log(`Inbox contains ${inboxCount} messages`);
+    return messageList;
+  } catch (error) {
+    console.error("Error occurred:", error);
+  }
+}
+
+//Marks messaged as read based on the message ID passed in
+async function markRead(msgID) {
+  try {
+    let markMsg = await markAsRead({
+      url: CONFIG.url,
+      mailboxID: CONFIG.senderMailboxID,
+      mailboxPassword: CONFIG.senderMailboxPassword,
+      sharedKey: CONFIG.sharedKey,
+      message: msgID,
+      agent: CONFIG.senderAgent,
+    });
+    return markMsg;
+  } catch (error) {
+    console.error("Error occurred:", error);
+  }
+}
+
+//Reads message data based on message ID
+async function readMsg(msgID) {
+  try {
+    let messages = await readMessage({
+      url: CONFIG.url,
+      mailboxID: CONFIG.senderMailboxID,
+      mailboxPassword: CONFIG.senderMailboxPassword,
+      sharedKey: CONFIG.sharedKey,
+      messageID: msgID,
+      agent: CONFIG.senderAgent,
+    });
+    return messages.data;
+  } catch (error) {
+    console.error("Error occurred:", error);
+  }
+}
+
 async function readSecret(secretName, client) {
   return Buffer.from(
     await getSecret(secretName, client),
