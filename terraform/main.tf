1b7fcac41f0ffb5a72dcbab0bca715e629fc7990
Abdul-NHS <abdul.shaker1@nhs.net>
2024-02-11 12:23:13 +0000

    feature/GAL-396# added skeleton for lambda on event

--- terraform/main.tf
+++ terraform/main.tf
@@ -88,6 +88,13 @@ module "invited_participant_batch" {
   environment             = var.environment
 }
 
+module "valid_clinic_schedule_summary_bucket" {
+  source                  = "./modules/s3"
+  bucket_name             = "valid-inbound-gtms-clinic-schedule-summary"
+  galleri_lambda_role_arn = module.iam_galleri_lambda_role.galleri_lambda_role_arn
+  environment             = var.environment
+}
+
 # Data Filter Gridall IMD
 module "data_filter_gridall_imd_lambda" {
   source               = "./modules/lambda"
@@ -690,6 +697,37 @@ module "create_invitation_batch_dynamodb_stream" {
   maximum_batching_window_in_seconds = 300
   filter_event_name                  = "INSERT"
 }
+
+# GTMS upload clinic data
+module "gtms_upload_clinic_capacity_data_lambda" {
+  source               = "./modules/lambda"
+  environment          = var.environment
+  bucket_id            = module.s3_bucket.bucket_id
+  lambda_iam_role      = module.iam_galleri_lambda_role.galleri_lambda_role_arn
+  lambda_function_name = "gtmsUploadClinicCapacityDataLambda"
+  lambda_timeout       = 100
+  memory_size          = 1024
+  lambda_s3_object_key = "gtms_upload_clinic_capacity_data_lambda.zip"
+  environment_vars = {
+    ENVIRONMENT = "${var.environment}"
+  }
+}
+
+module "gtms_upload_clinic_capacity_data_cloudwatch" {
+  source               = "./modules/cloudwatch"
+  environment          = var.environment
+  lambda_function_name = module.gtms_upload_clinic_capacity_data.lambda_function_name
+  retention_days       = 14
+}
+
+module "gtms_upload_clinic_capacity_data_trigger" {
+  source        = "./modules/lambda_trigger"
+  bucket_id     = module.valid_clinic_schedule_summary_bucket.bucket_id
+  bucket_arn    = module.valid_clinic_schedule_summary_bucket.bucket_arn
+  lambda_arn    = module.gtms_upload_clinic_capacity_data.lambda_arn
+  filter_prefix = "validRecords/valid_records_add-"
+}
+
 
 # Dynamodb tables
 module "sdrs_table" {
