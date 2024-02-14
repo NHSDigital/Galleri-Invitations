eb188814c21da4925567ba854454831df44facd3
umayrsNHS <umayr.saghir1@nhs.net>
2024-02-14 14:26:27 +0000

    clarify comment to highlight json contains the curated data

--- scripts/pipeline/seed-data.sh
+++ scripts/pipeline/seed-data.sh
@@ -33,7 +33,7 @@ function main() {
   if [[ $environment_type == "dev" || "test" ]]; then
     sed -i "s/ENVIRONMENT/$environment/g" $GITHUB_WORKSPACE/scripts/test_data/destructible_environments/lsoa.json
     aws dynamodb batch-write-item  --request-items file://$GITHUB_WORKSPACE/scripts/test_data/destructible_environments/lsoa.json
-    # LSOA table needs a minimum of 1MB of data for lambda to function, hence as a workaround populating table with cut down CSV file in addition to lsoa json
+    # LSOA table needs a minimum of 1MB of data for lambda to function, hence as a workaround populating table with cut down CSV file with uncurated padding records in addition to lSOA json with curated data.
     mkdir nonprod-unique-lsoa-data
     aws s3 cp s3://galleri-ons-data/lsoa_data/destructible_environments/unique_lsoa_data.csv ./nonprod-unique-lsoa-data
     python $PWD/scripts/pipeline/nonprod_unique_lsoa_load/nonprod_unique_lsoa_load.py
