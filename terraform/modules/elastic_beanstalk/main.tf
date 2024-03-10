8134f7cb885e9a990fdd6e2ca0a13f514b1ecfbc
Andrew Cleveland <andrew.cleveland1@nhs.net>
2024-03-10 16:12:24 +0000

    removed Iam permissions

--- terraform/modules/elastic_beanstalk/main.tf
+++ terraform/modules/elastic_beanstalk/main.tf
@@ -14,56 +14,56 @@ data "archive_file" "screens" {
   output_path = "${path.cwd}/src/${var.name}.zip"
 }
 
-# DNS setup
+# # DNS setup
 
-resource "aws_route53_record" "screens" {
-  zone_id = data.aws_route53_zone.domain.id
-  name    = "${var.dns_zone}.${var.hostname}"
-  type    = "A"
+# resource "aws_route53_record" "screens" {
+#   zone_id = data.aws_route53_zone.domain.id
+#   name    = "${var.dns_zone}.${var.hostname}"
+#   type    = "A"
 
-  alias {
-    name                   = aws_elastic_beanstalk_environment.screens.cname
-    zone_id                = data.aws_route53_zone.domain.id
-    evaluate_target_health = true
-  }
-}
+#   alias {
+#     name                   = aws_elastic_beanstalk_environment.screens.cname
+#     zone_id                = data.aws_route53_zone.domain.id
+#     evaluate_target_health = true
+#   }
+# }
 
-resource "aws_acm_certificate" "my_cert" {
-  domain_name       = "${var.dns_zone}.${var.hostname}"
-  validation_method = "DNS"
+# resource "aws_acm_certificate" "my_cert" {
+#   domain_name       = "${var.dns_zone}.${var.hostname}"
+#   validation_method = "DNS"
 
-  subject_alternative_names = ["www.${var.dns_zone}.${var.hostname}"]
+#   subject_alternative_names = ["www.${var.dns_zone}.${var.hostname}"]
 
-  lifecycle {
-    create_before_destroy = true
-  }
+#   lifecycle {
+#     create_before_destroy = true
+#   }
 
-  tags = {
-    Environment = var.environment
-  }
-}
+#   tags = {
+#     Environment = var.environment
+#   }
+# }
 
-resource "aws_route53_record" "cert_validation" {
-  # dvo = domain validation option
-  for_each = {
-    for dvo in aws_acm_certificate.my_cert.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
+# resource "aws_route53_record" "cert_validation" {
+#   # dvo = domain validation option
+#   for_each = {
+#     for dvo in aws_acm_certificate.my_cert.domain_validation_options : dvo.domain_name => {
+#       name   = dvo.resource_record_name
+#       record = dvo.resource_record_value
+#       type   = dvo.resource_record_type
+#     }
+#   }
 
-  zone_id = data.aws_route53_zone.domain.id
-  name    = each.value.name
-  type    = each.value.type
-  records = [each.value.record]
-  ttl     = 60
-}
+#   zone_id = data.aws_route53_zone.domain.id
+#   name    = each.value.name
+#   type    = each.value.type
+#   records = [each.value.record]
+#   ttl     = 60
+# }
 
-resource "aws_acm_certificate_validation" "my_cert" {
-  certificate_arn         = aws_acm_certificate.my_cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
-}
+# resource "aws_acm_certificate_validation" "my_cert" {
+#   certificate_arn         = aws_acm_certificate.my_cert.arn
+#   validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+# }
 
 # Elastic Beanstalk setup
 
