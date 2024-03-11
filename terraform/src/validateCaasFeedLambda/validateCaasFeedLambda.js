de4fc5fe4ca25bfbd29c3ace06b4629193ce5f11
NeerjaGoyalNHS <neerja.goyal1@nhs.net>
2024-03-11 14:25:44 +0000

    sonarcloud fix

--- terraform/src/validateCaasFeedLambda/validateCaasFeedLambda.js
+++ terraform/src/validateCaasFeedLambda/validateCaasFeedLambda.js
@@ -304,8 +304,8 @@ export function isValidNHSNumberFormat(nhsNumber) {
 
 export function isValidNameFormat(given_name, limit, flag) {
   //Check Valid name format
-  const isValidFormat = /^[\wàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð,.'\- ]+$/gim.test(given_name);
-  const otherNameValidFormat = /^[\wàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð,.'\-| ]+$/gim.test(given_name);
+  const isValidFormat = /^[\wŽžÀ-ÿ ,.'\-]+$/gim.test(given_name);
+  const otherNameValidFormat = /^[\wŽžÀ-ÿ ,.'\-|]+$/gim.test(given_name);
   if (given_name.split("").length > limit) {
     return false;
   }
