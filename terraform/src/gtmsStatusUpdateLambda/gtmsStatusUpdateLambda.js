cdae8058107a2a2505a3aeeeaaf2a7d1e8c6461d
Abdul-NHS <abdul.shaker1@nhs.net>
2024-03-12 16:44:53 +0000

    feature/GAL-152# added update func

--- terraform/src/gtmsStatusUpdateLambda/gtmsStatusUpdateLambda.js
+++ terraform/src/gtmsStatusUpdateLambda/gtmsStatusUpdateLambda.js
@@ -8,6 +8,7 @@ import {
   DynamoDBClient,
   GetItemCommand,
   QueryCommand,
+  UpdateItemCommand,
 } from "@aws-sdk/client-dynamodb";
 
 //VARIABLES
@@ -30,6 +31,7 @@ export const handler = async (event, context) => {
 
 
     const PERSON_ID = js?.['Withdrawal']?.['ParticipantID'];
+    const REASON = js?.['Withdrawal']?.['Reason'];
     console.log(PERSON_ID); //NHS-AC35-BS33
 
     const retrievedBatchID = await lookupParticipantId(PERSON_ID, 'Episode', client, ENVIRONMENT);
@@ -46,6 +48,8 @@ export const handler = async (event, context) => {
           Episode Event Updated By = GTMS
           Episode Status = Closed
         */
+        const saved = await saveObjToEpisodeTable(csvString, ENVIRONMENT, client, retrievedBatchID[0], REASON, PERSON_ID);
+        console.log(saved);
       } else {
         console.log('fail');
         //AC1c: Save rejected record folder: 'episode does not exist'
@@ -136,7 +140,7 @@ export const lookupParticipantId = async (participantId, table, dbClient, enviro
     return [""];
   }
   console.log('match');
-  return [response["Items"][0]["Participant_Id"]["S"]];
+  return [response["Items"][0]["Batch_Id"]["S"]];
 };
 
 //Used to lookup participant from population table
@@ -162,5 +166,67 @@ export const lookupParticipant = async (participantId, table, dbClient, environm
   } else {
     console.log('match');
     return true;
+  }
+};
+
+export const saveObjToEpisodeTable = async (csvString, environment, client, batchId, reason, personId) => {
+  const createTime = String(Date.now())
+  const params = {
+    "Key": {
+      "Batch_Id": {
+        "S": batchId,
+      },
+      "Participant_Id": {
+        "S": personId,
+      }
+    },
+    ExpressionAttributeNames: {
+      "#EE": "Episode_Event",
+      "#EEU": "Episode_Event_Updated",
+      "#ES": "Episode_Status",
+      "#ESU": "Episode_Status_Updated",
+      "#EEUB": "Episode_Event_Updated_By",
+      "#EED": "Episode_Event_Description",
+      "#EEN": "Episode_Event_Notes",
+    },
+    ExpressionAttributeValues: {
+      ":Episode_Event_new": {
+        "S": "Withdrawn",
+      },
+      ":Episode_Event_Updated_new": {
+        "S": createTime,
+      },
+      ":Episode_Status_new": {
+        "S": "Closed",
+      },
+      ":Episode_Status_Updated_new": {
+        "S": createTime,
+      },
+      ":Episode_Event_Updated_By_new": {
+        "S": "GTMS",
+      },
+      ":Episode_Event_Description_new": {
+        "S": reason,
+      },
+      ":Episode_Event_Notes_new": {
+        "S": "NULL",
+      },
+    },
+    TableName: `${environment}-Episode`,
+    UpdateExpression: "SET #EE = :Episode_Event_new, #EEU = :Episode_Event_Updated_new,  #ES = :Episode_Status_new, #ESU = :Episode_Status_Updated_new, #EEUB = :Episode_Event_Updated_By_new, #EED = :Episode_Event_Description_new, #EEN = :Episode_Event_Notes_new"
+  };
+
+  const command = new UpdateItemCommand(params);
+  try {
+    const response = await client.send(command);
+    if (response.$metadata.httpStatusCode !== 200) {
+      console.error(`Error updating item: ${csvString}`);
+      return false;
+    } else {
+      console.log(`Successfully updated with item: ${csvString}`);
+      return true;
+    }
+  } catch (error) {
+    console.error(`Error: ${error}`);
   }
 };
