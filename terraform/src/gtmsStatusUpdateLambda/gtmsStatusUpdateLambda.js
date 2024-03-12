b12ff081263bb3eae0a3e5a4ecca9d11a9e9b2e5
Abdul-NHS <abdul.shaker1@nhs.net>
2024-03-12 11:02:27 +0000

    feature/GAL-152# adding base search and deploying

--- terraform/src/gtmsStatusUpdateLambda/gtmsStatusUpdateLambda.js
+++ terraform/src/gtmsStatusUpdateLambda/gtmsStatusUpdateLambda.js
@@ -5,6 +5,7 @@ import {
 } from "@aws-sdk/client-s3";
 import {
   DynamoDBClient,
+  GetItemCommand,
   ScanCommand,
   BatchWriteItemCommand,
   UpdateItemCommand
@@ -20,6 +21,49 @@ export const handler = async (event, context) => {
   const bucket = event.Records[0].s3.bucket.name;
   const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
   console.log(`Triggered by object ${key} in bucket ${bucket}`);
+  try {
+    const csvString = await readCsvFromS3(bucket, key, s3);
+    const js = JSON.parse(csvString);
+    console.log(`js variable: , ${js}`)
+
+    // const PERSON_ID = ;
+    // const result = await getItemsFromTable(
+    //   `${ENVIRONMENT}-Population`,
+    //   client,
+    //   PERSON_ID,
+    // );
+  } catch (error) {
+    console.error("Error occurred:", error);
+  }
 }
 
 //FUNCTIONS
+export const readCsvFromS3 = async (bucketName, key, client) => {
+  try {
+    const response = await client.send(
+      new GetObjectCommand({
+        Bucket: bucketName,
+        Key: key,
+      })
+    );
+    return response.Body.transformToString();
+  } catch (err) {
+    console.error(`Failed to read from ${bucketName}/${key}`);
+    throw err;
+  }
+};
+
+export async function getItemsFromTable(table, client, key) {
+  const params = {
+    Key: {
+      PersonId: {
+        N: `${key}`,
+      },
+    },
+    TableName: table,
+  };
+  const command = new GetItemCommand(params);
+  const response = await client.send(command);
+
+  return response;
+}
