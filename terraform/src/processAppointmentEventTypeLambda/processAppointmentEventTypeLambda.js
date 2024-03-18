f9dc45cb3739cbeac4c847c877aac2935691722d
Aymene Nibouche <aymene.nibouche1@nhs.net>
2024-03-18 17:39:22 +0000

    Basic switching logic

--- terraform/src/processAppointmentEventTypeLambda/processAppointmentEventTypeLambda.js
+++ terraform/src/processAppointmentEventTypeLambda/processAppointmentEventTypeLambda.js
@@ -8,12 +8,35 @@ export const handler = async (event) => {
   const bucket = record.s3.bucket.name;
   const key = decodeURIComponent(record.s3.object.key.replace(/\+/g, " "));
   console.log(`Triggered by object ${key} in bucket ${bucket}`);
-
   try {
-    console.log(`This is what the key looks like${key}`);
+    const eventType = await extractStringFromFilepath(key);
+    switch (eventType) {
+      case "update":
+        console.log(`This was a ${eventType}`);
+        break;
+      case "no_show":
+        console.log(`This was a ${eventType}`);
+        break;
+      case "aborted":
+        console.log(`This was a ${eventType}`);
+        break;
+      case null:
+        console.log(`This was a ${eventType}`);
+        break;
+    }
   } catch (error) {
-    const message = `Error processing object ${key} in bucket ${bucket}: ${err}`;
+    const message = `Error processing object ${key} in bucket ${bucket}: ${error}`;
     console.error(message);
     throw new Error(message);
   }
+};
+
+//METHODS
+export const extractStringFromFilepath = async (filepath) => {
+  const regex = /valid_records-([^-]+)-([^.]+)\.json/;
+  const match = filepath.match(regex);
+  if (match && match[1]) {
+    return match[1];
+  }
+  return null;
 };
