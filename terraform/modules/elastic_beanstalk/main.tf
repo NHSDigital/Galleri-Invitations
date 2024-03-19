c7eae6af713758d5e5072aa7f66c71f1d60f5f12
Andrew Cleveland <andrew.cleveland1@nhs.net>
2024-03-19 17:16:54 +0000

    changed cnames to decouple a record and elastic beanstalk

--- terraform/modules/elastic_beanstalk/main.tf
+++ terraform/modules/elastic_beanstalk/main.tf
@@ -42,8 +42,6 @@ resource "aws_route53_record" "cert_validation" {
 resource "aws_acm_certificate_validation" "cert_validation" {
   certificate_arn         = aws_acm_certificate.cert.arn
   validation_record_fqdns = [for _, record in aws_route53_record.cert_validation : "${record.name}.${var.hostname}."]
-
-  depends_on = [aws_route53_record.cert_validation]
 }
 
 resource "aws_acm_certificate" "cert" {
