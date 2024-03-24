617bad51f2c742d92189c61e955ec212ae59bd09
Abdul-NHS <abdul.shaker1@nhs.net>
2024-03-24 13:05:39 +0000

    feature/GAL-718#typo

--- terraform/src/AppointmentEventCancelledLambda/AppointmentEventCancelledLambda.js
+++ terraform/src/AppointmentEventCancelledLambda/AppointmentEventCancelledLambda.js
@@ -22,7 +22,18 @@ export const handler = async (event) => {
   const key = decodeURIComponent(record.s3.object.key.replace(/\+/g, " "));
   console.log(`Triggered by object ${key} in bucket ${bucket}`);
 
+  const csvString = await readCsvFromS3(bucket, key, s3);
+  const js = JSON.parse(csvString); //convert string retrieved from S3 to object
 
+  console.log(js);
+  // const episodeResponse = await lookUp(
+  //   dbClient,
+  //   ParticipantID,
+  //   "Episode",
+  //   "Participant_Id",
+  //   "S",
+  //   true
+  // );
 
 
 }
@@ -59,4 +70,35 @@ export const pushCsvToS3 = async (bucketName, key, body, client) => {
     console.log(`Failed to push to ${bucketName}/${key}. Error Message: ${err}`);
     throw err;
   }
+};
+
+export const lookUp = async (dbClient, ...params) => {
+  const [id, table, attribute, attributeType, useIndex] = params;
+
+  const ExpressionAttributeValuesKey = `:${attribute}`;
+  let expressionAttributeValuesObj = {};
+  let expressionAttributeValuesNestObj = {};
+
+  expressionAttributeValuesNestObj[attributeType] = id;
+  expressionAttributeValuesObj[ExpressionAttributeValuesKey] =
+    expressionAttributeValuesNestObj;
+
+  const input = {
+    ExpressionAttributeValues: expressionAttributeValuesObj,
+    KeyConditionExpression: `${attribute} = :${attribute}`,
+    TableName: `${ENVIRONMENT}-${table}`,
+  };
+
+  if (useIndex) {
+    input.IndexName = `${attribute}-index`;
+  }
+
+  const getCommand = new QueryCommand(input);
+  const response = await dbClient.send(getCommand);
+
+  if (response.$metadata.httpStatusCode != 200) {
+    console.log(`look up item input = ${JSON.stringify(input, null, 2)}`);
+  }
+
+  return response;
 };
