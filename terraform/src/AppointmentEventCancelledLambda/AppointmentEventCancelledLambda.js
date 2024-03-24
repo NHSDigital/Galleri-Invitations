8aca48b21c774d27c1dc423a3112204e0139845a
Abdul-NHS <abdul.shaker1@nhs.net>
2024-03-24 12:22:28 +0000

    feature/GAL-718# adding lambda base

---
+++ terraform/src/AppointmentEventCancelledLambda/AppointmentEventCancelledLambda.js
@@ -0,0 +1,62 @@
+//IMPORTS
+import {
+  DynamoDBClient,
+  TransactWriteItemsCommand,
+  QueryCommand,
+} from "@aws-sdk/client-dynamodb";
+import {
+  S3Client,
+  GetObjectCommand,
+  PutObjectCommand,
+} from "@aws-sdk/client-s3";
+
+//VARIABLES
+const dbClient = new DynamoDBClient();
+const s3 = new S3Client();
+const ENVIRONMENT = process.env.ENVIRONMENT;
+
+//HANDLER
+export const handler = async (event) => {
+  const record = event.Records[0];
+  const bucket = record.s3.bucket.name;
+  const key = decodeURIComponent(record.s3.object.key.replace(/\+/g, " "));
+  console.log(`Triggered by object ${key} in bucket ${bucket}`);
+
+
+
+
+}
+
+//FUNCTIONS
+export const readCsvFromS3 = async (bucketName, key, client) => {
+  try {
+    const response = await client.send(
+      new GetObjectCommand({
+        Bucket: bucketName,
+        Key: key,
+      })
+    );
+    return response.Body.transformToString();
+  } catch (err) {
+    console.error(`Failed to read from ${bucketName}/${key}`);
+    throw err;
+  }
+};
+
+export const pushCsvToS3 = async (bucketName, key, body, client) => {
+  try {
+    const response = await client.send(
+      new PutObjectCommand({
+        Bucket: bucketName,
+        Key: key,
+        Body: body,
+      })
+    );
+
+    console.log(`Successfully pushed to ${bucketName}/${key}`);
+    return response;
+  } catch (err) {
+    console.log(`Failed to push to ${bucketName}/${key}. Error Message: ${err}`);
+    throw err;
+  }
+};
