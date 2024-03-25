121a6e4a0d3344171f3b392be06e8301c3edf2b3
Andrew Cleveland <andrew.cleveland1@nhs.net>
2024-03-25 09:40:38 +0000

    updated files

--- scripts/pipeline/seed-data.sh
+++ scripts/pipeline/seed-data.sh
@@ -120,11 +120,8 @@ function main() {
       echo Initiating upload of dummy Population data to database
       mkdir nonprod-population-data
       # aws s3 cp s3://$environment_type-galleri-test-data/non_prod_participant_data/ ./nonprod-population-data --recursive
-      if [[ $environment_type == "dev" || $environment_type == "test" ]]; then
-        unzip $PWD/scripts/test_data/lsoa/trimmed/unique_lsoa_data.zip -d ./nonprod-population-data
-      else
-        unzip $PWD/scripts/test_data/lsoa/untrimmed/unique_lsoa_data.zip -d ./nonprod-population-data
-      fi
+      unzip $PWD/scripts/test_data/female_participants_with_LSOA_Invited.zip -d ./nonprod-population-data
+      unzip $PWD/scripts/test_data/male_participants_with_LSOA_Invited.zip -d ./nonprod-population-data
       echo Succefully Downloaded galleri-test-data CSVs from S3
       echo Uploading items to Population database
       python $PWD/scripts/pipeline/nonprod_population_load/nonprod_population_load.py
