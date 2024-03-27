6e9ba36570fad32d326af41babf13ff58b9cbe58
Abdul-NHS <abdul.shaker1@nhs.net>
2024-03-27 12:22:42 +0000

    feature/GAL-404# added validation rules and updated incorrect supplied schema

--- terraform/src/validateGtmsWithdrawalLambda/validateGtmsWithdrawalLambda.js
+++ terraform/src/validateGtmsWithdrawalLambda/validateGtmsWithdrawalLambda.js
@@ -5,7 +5,7 @@ import {
   S3Client,
 } from "@aws-sdk/client-s3";
 import { validate } from "jsonschema";
-import schema from "./GTMS-schema.json" assert { type: "json" };
+import schema from "./GTMS-withdrawal-schema.json" assert { type: "json" };
 
 //VARIABLES
 const s3 = new S3Client();
@@ -18,8 +18,45 @@ export const handler = async (event) => {
   const key = decodeURIComponent(record.s3.object.key.replace(/\+/g, " "));
   console.log(`Triggered by object ${key} in bucket ${bucket}`);
 
+  try {
+    const jsonString = await readCsvFromS3(bucket, key, s3);
+    const validateResult = validate(JSON.parse(jsonString), schema); //convert string retrieved from S3 to object
 
-}
+    console.log(`Finished validating object ${key} in bucket ${bucket}`);
+    console.log(
+      "----------------------------------------------------------------"
+    );
+
+    console.log(
+      `Pushing filtered valid records and invalid records to their respective sub-folder in bucket ${bucket}`
+    );
+    console.log(validateResult.valid);
+
+    if (validateResult.valid) {
+      await pushCsvToS3(
+        `${ENVIRONMENT}-processed-inbound-gtms-withdrawal`,
+        `validRecords/valid_records_add-${Date.now()}.json`,
+        jsonString,
+        s3
+      );
+    } else {
+      console.warn(
+        "PLEASE FIND THE INVALID Clinic RECORDS FROM THE PROCESSED Clinic Data BELOW:\n" +
+        validateResult.errors,
+      );
+      await pushCsvToS3(
+        `${ENVIRONMENT}-processed-inbound-gtms-withdrawal`,
+        `failed_validation/invalid_records_add-${Date.now()}.json`,
+        jsonString,
+        s3
+      );
+    }
+  } catch (err) {
+    const message = `Error processing object ${key} in bucket ${bucket}: ${err}`;
+    console.error(message);
+    throw new Error(message);
+  }
+};
 
 
 //FUNCTIONS
@@ -54,4 +91,4 @@ export const pushCsvToS3 = async (bucketName, key, body, client) => {
     console.log(`Failed to push to ${bucketName}/${key}. Error Message: ${err}`);
     throw err;
   }
-}; s
+};
