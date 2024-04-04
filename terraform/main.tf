db76243dfe0300e3bcee66f8f69b72b740a87963
Andrew Cleveland <andrew.cleveland1@nhs.net>
2024-04-04 10:03:09 +0100

    got initial eks working

--- terraform/main.tf
+++ terraform/main.tf
@@ -66,6 +66,13 @@ module "iam_galleri_lambda_role" {
 #   environment = var.environment
 # }
 
+# Setups up an eks cluster we can use to host the mesh sandbox for test environments and fhir validator
+module "eks" {
+  source      = "./modules/eks"
+  environment = var.environment
+  subnet_ids  = module.vpc.fargate_subnet_ids
+}
+
 module "s3_bucket" {
   source                  = "./modules/s3"
   bucket_name             = var.bucket_name
---
+++ terraform/modules/eks/eks.tf
@@ -0,0 +1,77 @@
+provider "kubernetes" {
+}
+
+resource "aws_eks_cluster" "cluster" {
+  name     = "${var.environment}-${var.name}"
+  role_arn = aws_iam_role.eks.arn
+
+  vpc_config {
+    subnet_ids = var.subnet_ids
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.eks_AmazonEKSServicePolicy,
+  ]
+}
+
+resource "aws_eks_fargate_profile" "cluster" {
+  cluster_name           = aws_eks_cluster.cluster.name
+  fargate_profile_name   = "${var.environment}-${var.name}"
+  pod_execution_role_arn = aws_iam_role.fargate_pod_execution.arn
+  subnet_ids             = var.subnet_ids
+
+  selector {
+    namespace = "default"
+  }
+}
+
+resource "aws_iam_role" "eks" {
+  name = "${var.environment}_${var.name}_eks_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks.name
+}
+
+resource "aws_iam_role" "fargate_pod_execution" {
+  name = "${var.environment}_${var.name}_fargate_pod_execution_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "eks-fargate-pods.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "fargate_pod_execution_AmazonEKSFargatePodExecutionRolePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
+  role       = aws_iam_role.fargate_pod_execution.name
+}
+
+
+resource "aws_iam_role_policy_attachment" "eks_AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.eks.name
+}
