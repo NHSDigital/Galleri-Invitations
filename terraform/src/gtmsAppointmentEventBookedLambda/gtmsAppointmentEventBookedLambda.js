78a9a8ae3a6d78a067c518da3fd7141ba2baa136
Abdul-NHS <abdul.shaker1@nhs.net>
2024-04-05 11:02:27 +0100

    feature/GAL-1195# lambda code template with transactWrite

---
+++ terraform/src/gtmsAppointmentEventBookedLambda/gtmsAppointmentEventBookedLambda.js
@@ -0,0 +1,156 @@
+//IMPORTS
+import {
+  DynamoDBClient,
+  TransactWriteItemsCommand,
+  QueryCommand,
+} from "@aws-sdk/client-dynamodb";
+import {
+  S3Client,
+  GetObjectCommand,
+  PutObjectCommand,
+} from "@aws-sdk/client-s3";
+
+//VARIABLES
+const dbClient = new DynamoDBClient();
+const s3 = new S3Client();
+const ENVIRONMENT = process.env.ENVIRONMENT;
+//Need a lambda param for appointmentdatetime modifier
+
+//HANDLER
+export const handler = async (event) => {
+  const record = event.Records[0];
+  const bucket = record.s3.bucket.name;
+  const key = decodeURIComponent(record.s3.object.key.replace(/\+/g, " "));
+  console.log(`Triggered by object ${key} in bucket ${bucket}`);
+
+  const csvString = await readCsvFromS3(bucket, key, s3);
+  const js = JSON.parse(csvString); //convert string retrieved from S3 to object
+
+}
+
+
+//FUNCTIONS
+export const readCsvFromS3 = async (bucketName, key, client) => {
+  try {
+    const response = await client.send(
+      new GetObjectCommand({
+        Bucket: bucketName,
+        Key: key,
+      })
+    );
+    return response.Body.transformToString();
+  } catch (err) {
+    console.error(`Failed to read from ${bucketName}/${key}`);
+    throw err;
+  }
+};
+
+export const pushCsvToS3 = async (bucketName, key, body, client) => {
+  try {
+    const response = await client.send(
+      new PutObjectCommand({
+        Bucket: bucketName,
+        Key: key,
+        Body: body,
+      })
+    );
+
+    console.log(`Successfully pushed to ${bucketName}/${key}`);
+    return response;
+  } catch (err) {
+    console.log(`Failed to push to ${bucketName}/${key}. Error Message: ${err}`);
+    throw err;
+  }
+};
+
+export const lookUp = async (dbClient, ...params) => {
+  const [id, table, attribute, attributeType, useIndex] = params;
+
+  const ExpressionAttributeValuesKey = `:${attribute}`;
+  let expressionAttributeValuesObj = {};
+  let expressionAttributeValuesNestObj = {};
+
+  expressionAttributeValuesNestObj[attributeType] = id;
+  expressionAttributeValuesObj[ExpressionAttributeValuesKey] =
+    expressionAttributeValuesNestObj;
+
+  const input = {
+    ExpressionAttributeValues: expressionAttributeValuesObj,
+    KeyConditionExpression: `${attribute} = :${attribute}`,
+    TableName: `${ENVIRONMENT}-${table}`,
+  };
+
+  if (useIndex) {
+    input.IndexName = `${attribute}-index`;
+  }
+
+  const getCommand = new QueryCommand(input);
+  const response = await dbClient.send(getCommand);
+
+  if (response.$metadata.httpStatusCode != 200) {
+    console.log(`look up item input = ${JSON.stringify(input, null, 2)}`);
+  }
+
+  return response;
+};
+
+export const transactionalWrite = async (
+  client,
+  participantId,
+  batchId,
+  appointmentId,
+  eventType,
+  episodeEvent,
+  cancellationReason,
+) => {
+  const timeNow = String(Date.now());
+  const params = {
+    TransactItems: [
+      {
+        Update: {
+          Key: {
+            Batch_Id: { S: batchId },
+            Participant_Id: { S: participantId },
+          },
+          UpdateExpression: `SET Episode_Event = :episodeEvent, Episode_Event_Updated = :timeNow, Episode_Event_Description = :eventDescription, Episode_Status = :open, Episode_Event_Notes = :null, Episode_Event_Updated_By = :gtms, Episode_Status_Updated = :timeNow`,
+          TableName: `${ENVIRONMENT}-Episode`,
+          ExpressionAttributeValues: {
+            ":episodeEvent": { S: episodeEvent },
+            ":timeNow": { N: timeNow },
+            ":eventDescription": { S: cancellationReason },
+            ":open": { S: "Open" },
+            ":null": { S: "Null" },
+            ":gtms": { S: "GTMS" },
+          },
+        },
+      },
+      {
+        Update: {
+          Key: {
+            Participant_Id: { S: participantId },
+            Appointment_Id: { S: appointmentId },
+          },
+          UpdateExpression: `SET event_type = :eventType`,
+          TableName: `${ENVIRONMENT}-Appointments`,
+          ExpressionAttributeValues: {
+            ":eventType": { S: eventType },
+          },
+        },
+      },
+    ],
+  };
+
+  try {
+    const command = new TransactWriteItemsCommand(params);
+    const response = await client.send(command);
+    if (response.$metadata.httpStatusCode !== 200) {
+      console.error(`Error occurred while trying to update db with item: ${participantId}`);
+      return false;
+    } else {
+      console.log(`Successfully updated db with item: ${participantId}`);
+      return true;
+    }
+  } catch (error) {
+    console.error("Transactional write failed:", error);
+  }
+};
