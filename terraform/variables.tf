02f50a546ab84a9f4ddcd0a72a901f03415f0d28
Aymene Nibouche <aymene.nibouche1@nhs.net>
2024-04-15 16:28:14 +0100

    Removed variable changes

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -14,7 +14,7 @@ variable "environment" {
 
 variable "frontend_repo_location" {
   description = "The location of the Galler-Frontend repo on the filesystem"
-  default     = "/Users/aymene/Development/Galleri/Galleri-Frontend"
+  default     = "/home/runner/work/Galleri-Invitations/Galleri-Invitations/Galleri-Frontend"
 }
 
 variable "USERS" {
@@ -49,7 +49,6 @@ variable "GALLERI_ACTIVITY_NAME" {
 
 variable "account_id" {
   description = "account id"
-  default     = 136293001324
 }
 
 variable "CIS2_TOKEN_ENDPOINT_URL" {
