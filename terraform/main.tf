d7222712c490235aa03434f451773599ecb72b57
Andrew Cleveland <andrew.cleveland1@nhs.net>
2024-04-16 09:12:07 +0100

    updated permissions

--- terraform/main.tf
+++ terraform/main.tf
@@ -121,13 +121,13 @@ module "eks" {
         eksAdmin = {
           policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSAdminPolicy"
           access_scope = {
-            type       = "cluster"
+            type = "cluster"
           }
         },
         clusterAdmin = {
           policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
           access_scope = {
-            type       = "cluster"
+            type = "cluster"
           }
         },
       }
