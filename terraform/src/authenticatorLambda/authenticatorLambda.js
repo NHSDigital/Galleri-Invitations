c4c1dbcf42213172a3b45c740ea11c162bd43c53
Amar-NHS <amar.rai5@nhs.net>
2024-04-18 16:49:32 +0100

    SPIKE-CIS2# adding token and user endpoint call

--- terraform/src/authenticatorLambda/authenticatorLambda.js
+++ terraform/src/authenticatorLambda/authenticatorLambda.js
@@ -2,6 +2,7 @@ import jwt from "jsonwebtoken";
 import { v4 as uuidv4 } from "uuid";
 import { DynamoDBClient, GetItemCommand } from "@aws-sdk/client-dynamodb";
 import { unmarshall } from "@aws-sdk/util-dynamodb";
+import axios from "axios";
 
 import {
   GetSecretValueCommand,
@@ -15,8 +16,21 @@ const CLIENT_ID = process.env.CIS2_ID;
 const TOKEN_ENDPOINT_URL = process.env.CIS2_TOKEN_ENDPOINT_URL;
 const KID = process.env.CIS2_PUBLIC_KEY_ID;
 const PRIVATE_KEY_SECRET_NAME = process.env.CIS2_KEY_NAME;
+const CIS2_REDIRECT_URL = "http://localhost:3000/api/auth/callback/cis2";
 
 export const handler = async (event) => {
+  const code = event.queryStringParameters.code;
+  console.log("Code :", code);
+  const signedJWT = await getCIS2SignedJWT();
+  console.log("signedJWT :", signedJWT);
+  const { tokens } = await getTokens(code, signedJWT);
+  console.log("tokens :", tokens);
+  const userInfo = await getUserinfo(tokens);
+  console.log("userInfo :", userInfo);
+  return userInfo;
+};
+
+export async function getCIS2SignedJWT() {
   try {
     console.log("Getting CIS2 signed jwt");
     const privateKey = await getSecret(PRIVATE_KEY_SECRET_NAME, smClient);
@@ -36,7 +50,7 @@ export const handler = async (event) => {
     const responseObject = createResponse(500, error.message);
     return responseObject;
   }
-};
+}
 
 export const getSecret = async (secretName, client) => {
   try {
@@ -88,6 +102,49 @@ export const createResponse = (httpStatusCode, body) => {
   responseObject.body = body;
   return responseObject;
 };
+
+export async function getTokens(authCode, signedJWT) {
+  const body = {
+    grant_type: "authorization_code",
+    redirect_uri: CIS2_REDIRECT_URL,
+    client_id: process.env.CIS2_ID || "undefined",
+    client_assertion_type:
+      "urn:ietf:params:oauth:client-assertion-type:jwt-bearer",
+    client_assertion: signedJWT || "undefined",
+    code: authCode || "undefined",
+  };
+  const data = new URLSearchParams(body).toString();
+  try {
+    const r = await axios({
+      method: "POST",
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+      },
+      data,
+      url: `https://am.nhsint.auth-ptl.cis2.spineservices.nhs.uk:443/openam/oauth2/realms/root/realms/NHSIdentity/realms/Healthcare/access_token`,
+    });
+    return { tokens: r.data };
+  } catch (err) {
+    console.error(err);
+    throw new Error(err);
+  }
+}
+
+export async function getUserinfo(tokens) {
+  try {
+    const response = await axios({
+      method: "GET",
+      url: "https://am.nhsint.auth-ptl.cis2.spineservices.nhs.uk:443/openam/oauth2/realms/root/realms/NHSIdentity/realms/Healthcare/userinfo?schema=openid",
+      headers: {
+        Authorization: `Bearer ${tokens.access_token}`,
+      },
+    });
+    return response.data;
+  } catch (err) {
+    console.error(err);
+    throw new Error(err);
+  }
+}
 
 export async function getUserRole(event) {
   const uuid = event.queryStringParameters.uuid;
