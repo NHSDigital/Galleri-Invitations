33d68ed9c88319f5b087f8f1a12d6d0cedb35002
avi312NHS <aviraj.mandair1@nhs.net>
2024-04-22 17:19:40 +0100

    wip spike

--- terraform/src/sessionManagerLambda/sessionManagerLambda.js
+++ terraform/src/sessionManagerLambda/sessionManagerLambda.js
@@ -4,38 +4,59 @@ import { unmarshall } from "@aws-sdk/util-dynamodb";
 const dynamoDBClient = new DynamoDBClient({ region: "eu-west-2" });
 const environment = process.env.ENVIRONMENT;
 
-export async function handler(event) {
-  const id = event.queryStringParameters.id;
-
+async function retrieveSession(sessionId) {
   const params = {
     TableName: `${environment}-session`,
     Key: {
-      id: { S: id },
+      sessionId: { S: sessionId },
     },
   };
 
-  console.log("id from query string parameters is: ", id);
+  try {
+    const data = await dynamoDBClient.send(new GetItemCommand(params));
+    if (!data.Item) {
+      return null; // Session not found
+    }
+
+    const session = unmarshall(data.Item);
+
+    if (session.expirationTime < Date.now()) {
+      return null; // Session has expired
+    }
+
+    return session;
+  } catch (error) {
+    console.error("Error retrieving session:", error);
+    throw error;
+  }
+}
+
+export async function handler(event) {
+  if (event.httpMethod !== "POST") {
+    return {
+      statusCode: 405,
+      body: JSON.stringify({ message: "Method Not Allowed" }),
+    };
+  }
+
+  const { sessionId } = JSON.parse(event.body);
 
   try {
-    const command = new GetItemCommand(params);
-    const data = await dynamoDBClient.send(command);
+    const session = await retrieveSession(sessionId);
 
-    if (!data.Item) {
-      console.error("User not found");
+    if (!session) {
       return {
         statusCode: 404,
-        body: JSON.stringify({ message: "User not found" }),
+        body: JSON.stringify({ message: "Session not found or expired" }),
       };
     }
 
-    const item = unmarshall(data.Item);
-
     return {
       statusCode: 200,
-      body: JSON.stringify(item),
+      body: JSON.stringify(session),
     };
   } catch (error) {
-    console.error("Error getting item from DynamoDB:", error);
+    console.error("Error handling request:", error);
     return {
       statusCode: 500,
       body: JSON.stringify({ message: "Internal Server Error" }),
