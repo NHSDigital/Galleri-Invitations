9752014642156f3c64a953a0260c5fb805eec0b6
Abdulqadir Qasim <abdulqadir.qasim1@nhs.net>
2024-04-22 17:20:11 +0100

    tag with PR name or date as message

--- scripts/pipeline/version.sh
+++ scripts/pipeline/version.sh
@@ -2,9 +2,14 @@
 
 set -x
 
+PR_NAME=$1
 SKIP_VERSION_BUMP=false
 VERSION=$(git describe --tags --abbrev=0)
-VERSION_TO_BUMP=$1
+VERSION_TO_BUMP=$2
+
+if [ -z $PR_NAME ]; then
+    PR_NAME="New PR on $(date)"
+fi
 
 if [ -z $VERSION ]; then
     NEW_VERSION="0.1.0"
@@ -27,5 +32,5 @@ echo NEW VERSION - $NEW_VERSION
 
 git config --global user.name "galleri-invitations"
 git config --global user.email "galleri-invitations@noreply.github.com"
-git tag $NEW_VERSION -m "initial version"
+git tag $NEW_VERSION -m $PR_NAME
 git push origin $NEW_VERSION
