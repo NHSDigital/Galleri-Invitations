29040418533ce0ea157123226432629228562c7c
Andrew Cleveland <andrew.cleveland1@nhs.net>
2024-04-24 10:25:37 +0100

    updated training pipeline

--- terraform/modules/lambda_s3_trigger/variables.tf
+++ terraform/modules/lambda_s3_trigger/variables.tf
@@ -1,18 +1,3 @@
-# locals {
-#   // Extracts all Lambda ARNs and deduplicates them.
-#   unique_lambda_arns = toset([for trigger in var.triggers : trigger.lambda_arn])
-# }
-
-# variable "triggers" {
-#   description = "List of triggers for the Lambda function"
-#   type = list(object({
-#     lambda_arn    = string
-#     bucket_events = list(string)
-#     filter_prefix = string
-#     filter_suffix = string
-#   }))
-# }
-
 locals {
   unique_lambda_arns = { for trigger_id, trigger in var.triggers : trigger_id => trigger.lambda_arn }
 }
