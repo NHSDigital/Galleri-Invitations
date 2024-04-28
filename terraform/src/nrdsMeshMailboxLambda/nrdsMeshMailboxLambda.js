7b7092ab560a30eaaf48e63aee9ae76e4b28fd27
Abdul-NHS <abdul.shaker1@nhs.net>
2024-04-28 12:54:34 +0100

    feature/GAL-894# messages being read

--- terraform/src/nrdsMeshMailboxLambda/nrdsMeshMailboxLambda.js
+++ terraform/src/nrdsMeshMailboxLambda/nrdsMeshMailboxLambda.js
@@ -1,22 +1,29 @@
 //IMPORTS
-import { getSecret, pushCsvToS3, getHealthStatusCode, getMessageArray, markRead, readMsg } from "./helper.js"
-import { handShake, loadConfig, getMessageCount, readMessage, markAsRead } from "nhs-mesh-client";
-import { S3Client } from '@aws-sdk/client-s3';
+import {
+  getSecret,
+  pushCsvToS3,
+  getHealthStatusCode,
+  getMessageArray,
+  markRead,
+  readMsg,
+} from "./helper.js";
+import {
+  handShake,
+  loadConfig,
+  getMessageCount,
+  readMessage,
+  markAsRead,
+} from "nhs-mesh-client";
+import { S3Client } from "@aws-sdk/client-s3";
 import { SecretsManagerClient } from "@aws-sdk/client-secrets-manager";
 //VARIABLES
 const smClient = new SecretsManagerClient({ region: "eu-west-2" });
 const clientS3 = new S3Client({});
 
-
 const ENVIRONMENT = process.env.ENVIRONMENT;
 
 //HANDLER
 export const handler = async (event, context) => {
-
-  //TODO: Need to retrieve secret
-  // const MESH_GTMS_KEY = await readSecret("MESH_SENDER_KEY", smClient);
-
-  //TODO: correct config
   const CONFIG = await loadConfig({
     url: "",
     TestKey: process.env.MESH_SHARED_KEY,
@@ -26,15 +33,14 @@ export const handler = async (event, context) => {
     receiverMailboxID: process.env.MESH_RECEIVER_MAILBOX_ID,
     receiverMailboxPassword: process.env.MESH_RECEIVER_MAILBOX_PASSWORD,
   });
-  console.log(process.env.MESH_RECEIVER_MAILBOX_ID);
+
   const HANDSHAKE = handShake;
   const MSG_COUNT = getMessageCount;
   const MARKED = markAsRead;
   const READING_MSG = readMessage;
 
   try {
-
-    console.log('Establishing connection');
+    console.log("Establishing connection");
     let healthy = await getHealthStatusCode(CONFIG, HANDSHAKE);
     let keepProcessing = true;
     if (healthy === 200) {
@@ -45,11 +51,22 @@ export const handler = async (event, context) => {
       console.log(`messageArr: ${messageArr}`);
       if (messageArr.length > 0) {
         for (const element of messageArr) {
-          const dateTime = new Date(Date.now()).toISOString()
+          const dateTime = new Date(Date.now()).toISOString();
           let message = await readMsg(CONFIG, READING_MSG, element); //returns messages based on id, iteratively from message list arr
           console.log(message);
           console.log(typeof message);
-          // const response = await pushCsvToS3(`${ENVIRONMENT}-processed-nrds-data`, `record_${dateTime}.json`, message, clientS3);
+          const response = await pushCsvToS3(
+            `${ENVIRONMENT}-processed-nrds-data`,
+            `record_${dateTime}.json`,
+            JSON.stringify(message["data"]),
+            clientS3
+          );
+          if (response?.$metadata?.httpStatusCode === 200) {
+            const confirmation = await markRead(CONFIG, MARKED, element);
+            console.log(`${confirmation.status} ${confirmation.statusText}`);
+          } else {
+            console.log(`Failed to process this message`);
+          }
         }
       } else {
         console.log("No messages to process");
@@ -57,20 +74,14 @@ export const handler = async (event, context) => {
       }
       // }
     }
-
-
-
-
   } catch (error) {
     console.error("Error occurred:", error);
   }
-}
-
+};
 
 //FUNCTIONS
 export async function readSecret(secretName, client) {
-  return Buffer.from(
-    await getSecret(secretName, client),
-    "base64"
-  ).toString("utf8");
+  return Buffer.from(await getSecret(secretName, client), "base64").toString(
+    "utf8"
+  );
 }
