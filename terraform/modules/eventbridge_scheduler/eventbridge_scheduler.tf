69b1dcc88016c8e29e16716fb83817936f81177e
Aymene Nibouche <aymene.nibouche1@nhs.net>
2024-04-30 00:18:58 +0100

    Fixed error

--- terraform/modules/eventbridge_scheduler/eventbridge_scheduler.tf
+++ terraform/modules/eventbridge_scheduler/eventbridge_scheduler.tf
@@ -13,7 +13,7 @@ resource "aws_cloudwatch_event_target" "lambda_target" {
 resource "aws_lambda_permission" "allow_eventbridge" {
   statement_id  = "AllowExecutionFromEventBridge"
   action        = "lambda:InvokeFunction"
-  function_name = var.function_name
+  function_name = var.lambda_arn
   principal     = "events.amazonaws.com"
   source_arn    = aws_cloudwatch_event_rule.schedule_rule.arn
 }
