228c34e6fd72e89472c6f317eb6258d3d688600a
Abdul-NHS <abdul.shaker1@nhs.net>
2024-04-29 11:06:22 +0100

    feature/GAL-894# testing github actions

--- terraform/src/nrdsMeshMailboxLambda/nrdsMeshMailboxLambda.js
+++ terraform/src/nrdsMeshMailboxLambda/nrdsMeshMailboxLambda.js
@@ -25,7 +25,8 @@ const ENVIRONMENT = process.env.ENVIRONMENT;
 //HANDLER
 export const handler = async (event, context) => {
   const CONFIG = await loadConfig({
-    url: process.env.K8_URL,
+    // url: process.env.K8_URL,
+    url: "test",
     TestKey: process.env.MESH_SHARED_KEY,
     sandbox: "true",
     // senderMailboxID: process.env.MESH_RECEIVER_MAILBOX_ID,
