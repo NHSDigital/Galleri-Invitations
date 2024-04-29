8c92c0ecb9f707cf2b07e9cbc46b0f0981cc58af
Michael Weller <michael.weller1@nhs.net>
2024-04-29 11:30:17 +0100

    Switch node-fetch versions to be compatible with jest tests

--- terraform/src/tests/unitTests/sendSingleNotifyMessageLambda.test.js
+++ terraform/src/tests/unitTests/sendSingleNotifyMessageLambda.test.js
@@ -1,4 +1,52 @@
+import { v4 as uuidv4 } from 'uuid';
+import { generateMessageReference } from '../../sendSingleNotifyMessageLambda/sendSingleNotifyMessageLambda';
+
 describe('processRecords', () => {
 
 });
 
+describe('getSecret', () => {
+
+});
+
+describe('generateJWT', () => {
+
+});
+
+describe('getAccessToken', () => {
+
+});
+
+describe('putSuccessResponseIntoTable', () => {
+
+});
+
+describe('putFailedResponseIntoTable', () => {
+
+});
+
+describe('putItemIntoTable', () => {
+
+});
+
+describe('generateMessageReference', () => {
+  it('should return a unique message reference ID', async () => {
+    const messageReferenceId = await generateMessageReference();
+    expect(messageReferenceId).toBeTruthy();
+    expect(messageReferenceId).toHaveLength(36);
+  });
+
+  it('should return a different message reference ID each time it is called', async () => {
+    const messageReferenceId1 = await generateMessageReference();
+    const messageReferenceId2 = await generateMessageReference();
+    expect(messageReferenceId1).not.toBe(messageReferenceId2);
+  });
+});
+
+describe('sendSingleMessage', () => {
+
+});
+
+describe('deleteMessageInQueue', () => {
+
+});
