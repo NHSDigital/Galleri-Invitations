47f57bb5c9a163824afbb17cc3c2623f01e29086
Abdulqadir Qasim <abdulqadir.qasim1@nhs.net>
2024-05-02 16:53:36 +0100

    Remove Name tag from Route53 zone due to build errors

--- terraform/modules/elastic_beanstalk/main.tf
+++ terraform/modules/elastic_beanstalk/main.tf
@@ -12,10 +12,9 @@ data "archive_file" "screens" {
 data "aws_route53_zone" "example" {
   name         = "${var.hostname}."
   private_zone = false
-  # tags = {
-  #   ApplicationRole = "${var.application_role}"
-  #   Name            = "${var.environment} Route 53 Zone"
-  # }
+  tags = {
+    ApplicationRole = "${var.application_role}"
+  }
 }
 
 data "aws_secretsmanager_secret_version" "galleri_activity_code" {
