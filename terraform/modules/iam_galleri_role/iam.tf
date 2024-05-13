10efd3cc637b80553a0a2acc836e88ec591ea1f0
Michael Weller <michael.weller1@nhs.net>
2024-05-13 16:23:15 +0100

    Add comment to iam policy terraform file

--- terraform/modules/iam_galleri_role/iam.tf
+++ terraform/modules/iam_galleri_role/iam.tf
@@ -514,6 +514,7 @@ resource "aws_iam_policy" "iam_policy_for_get_lsoa_in_range_lambda" {
 # Added sendEnrichedMessageToNotifyQueueLambda to this policy as lambda role exceeded policy limit
 # Added sendSingleNotifyMessageLambda to this policy as lambda role exceeded policy limit
 # Added nrdsMeshMailboxLambda to this policy as lambda role exceeded policy limit
+# Added testResultReportFhirValidationLambda to this policy as lambda role exceeded policy limit
 resource "aws_iam_policy" "iam_policy_for_participants_in_lsoa_lambda" {
   name        = "${var.environment}-aws_iam_policy_for_terraform_aws_participants_in_lsoa_lambda_role"
   path        = "/"
