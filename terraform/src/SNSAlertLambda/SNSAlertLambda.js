57548b769e4c114c322a8d90dc1b285ce758ad00
Andrew Cleveland <andrew.cleveland1@nhs.net>
2024-05-14 14:51:21 +0100

    updated lambda so it will generate errors correctly

--- terraform/src/SNSAlertLambda/SNSAlertLambda.js
+++ terraform/src/SNSAlertLambda/SNSAlertLambda.js
@@ -1,23 +1,41 @@
 export async function handler(event) {
   console.log("Received event:", JSON.stringify(event, null, 2));
 
-  const message = JSON.parse(event.Records[0].Sns.Message);
-  const alarmName = message.AlarmName;
-  const newState = message.NewStateValue;
-  const reason = message.NewStateReason;
-  const description = message.AlarmDescription;
-
-  const url = process.env.url;
-  const data = {
-    type: "AdaptiveCard",
-    version: "1.2",
-    body: {
-      type: "TextBlock",
-      text: `Galleri alarm test! \n${alarmName}\n${newState}\n${reason},${description}`,
-    },
-  };
-
   try {
+    const message = JSON.parse(event.Records[0].Sns.Message);
+
+    // Validate required fields
+    if (
+      !message.AlarmName ||
+      !message.NewStateValue ||
+      !message.NewStateReason ||
+      !message.AlarmDescription
+    ) {
+      throw new Error("Error: Invalid input, Missing required fields", message);
+    }
+
+    const alarmName = message.AlarmName;
+    const newState = message.NewStateValue;
+    const reason = message.NewStateReason;
+    const description = message.AlarmDescription;
+
+    const url = process.env.url;
+    if (!url) {
+      throw new Error(
+        "Error: Invalid input, URL is not set in environment variables",
+        message.url
+      );
+    }
+
+    const data = {
+      type: "AdaptiveCard",
+      version: "1.2",
+      body: {
+        type: "TextBlock",
+        text: `Galleri alarm test! \n${alarmName}\n${newState}\n${reason},${description}`,
+      },
+    };
+
     const response = await fetch(url, {
       method: "POST",
       headers: {
@@ -27,7 +45,7 @@ export async function handler(event) {
     });
 
     if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
+      throw new Error(`Error: HTTP error! status: ${response.status}`);
     }
 
     const responseData = await response.json();
@@ -41,12 +59,12 @@ export async function handler(event) {
       }),
     };
   } catch (error) {
-    console.error("Error:", error);
+    console.error("Error:", error.message);
     return {
       statusCode: 500,
       body: JSON.stringify({
         message: "Error making HTTP request",
-        error: error.message,
+        error: `Error: ${error.message}`,
       }),
     };
   }
