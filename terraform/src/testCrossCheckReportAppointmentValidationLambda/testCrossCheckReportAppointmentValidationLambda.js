06678d81b11453cf7d9f1765e4cfa312232f4320
fais-2 <faisal.isse1@nhs.net>
2024-05-16 11:28:29 +0100

    Cross-check the TRR against appointment

--- terraform/src/testCrossCheckReportAppointmentValidationLambda/testCrossCheckReportAppointmentValidationLambda.js
+++ terraform/src/testCrossCheckReportAppointmentValidationLambda/testCrossCheckReportAppointmentValidationLambda.js
@@ -4,10 +4,8 @@ import {
   PutObjectCommand,
   S3Client,
 } from "@aws-sdk/client-s3";
-import {
-  SecretsManagerClient,
-  GetSecretValueCommand,
-} from "@aws-sdk/client-secrets-manager";
+import { DynamoDBClient, QueryCommand } from "@aws-sdk/client-dynamodb";
+import { SecretsManagerClient } from "@aws-sdk/client-secrets-manager";
 import axios from "axios";
 
 //VARIABLES
@@ -16,6 +14,7 @@ const smClient = new SecretsManagerClient({ region: "eu-west-2" });
 const CR_TRR_SUCCESSFUL_BUCKET = process.env.CR_TRR_SUCCESSFUL_BUCKET;
 const CR_TRR_UNSUCCESSFUL_BUCKET = process.env.CR_TRR_UNSUCCESSFUL_BUCKET;
 const ENVIRONMENT = process.env.ENVIRONMENT;
+const dbClient = new DynamoDBClient();
 
 //HANDLER
 export const handler = async (event, context) => {
@@ -46,7 +45,6 @@ export async function processTRR(
   originalBucket,
   s3
 ) {
-  console.log(`call validateTRR ${testCrossCheckResultReport}`);
   const validTRR = await validateTRR(testCrossCheckResultReport, reportName);
   if (validTRR) {
     await putTRRInS3Bucket(
@@ -67,10 +65,48 @@ export async function processTRR(
 }
 
 // Validate TRR
-export async function validateTRR(testCrossCheckResultReport, reportName) {
-  console.log(`testCrossCheckResultReport ${testCrossCheckResultReport}`);
-  console.log(`reportName ${reportName}`);
-  return true;
+export async function validateTRR(js, reportName) {
+  const {
+    Appointment: {
+      ParticipantID: payloadParticipantId,
+      AppointmentID: payloadAppointmentId,
+      AppointmentDateTime: appointmentDateTime,
+      Timestamp: payloadTimestamp,
+      GrailID: payloadGrailId,
+      BloodCollectionDate: payloadBloodCollectionDate,
+    },
+  } = js;
+
+  //bring back most recent appointment, with timestamp
+
+  const appointmentParticipant = await lookUp(
+    dbClient,
+    payloadParticipantId,
+    "Appointments",
+    "Participant_Id",
+    "S",
+    false
+  ); //Check participant has any appointments
+  const apptArr = appointmentParticipant?.Items;
+  const sortedApptParticipants = apptArr?.sort(function (x, y) {
+    return new Date(x?.["Timestamp"]?.["S"]) < new Date(y?.["Timestamp"]?.["S"])
+      ? 1
+      : -1;
+  });
+  const appointmentParticipantItems = sortedApptParticipants[0];
+
+  if (
+    appointmentParticipantItems?.Participant_Id.S === payloadParticipantId &&
+    appointmentParticipantItems?.grail_id.S === payloadGrailId &&
+    appointmentParticipantItems?.blood_collection_date.S ===
+      payloadBloodCollectionDate
+  ) {
+    console.log(`move TRR to the 'Step 3 validated successfully bucket'`);
+    return true;
+  } else {
+    console.log(`move TRR to the 'Step 3 validated unsuccessful bucket`);
+    return false;
+  }
 }
 
 // Move TRR to S3 bucket
@@ -163,18 +199,43 @@ export async function getJSONFromS3(bucketName, key, client) {
   }
 }
 
-//Return 'Secret value' from secrets manager by passing in 'Secret name'
-export const getSecret = async (secretName, client) => {
-  try {
-    const response = await client.send(
-      new GetSecretValueCommand({
-        SecretId: secretName,
-      })
-    );
-    console.log(`Retrieved value successfully ${secretName}`);
-    return response.SecretString;
-  } catch (error) {
-    console.error(`Error: Failed to retrieve ${secretName}`);
-    throw error;
+/**
+ * This function allows the user to query against DynamoDB.
+ *
+ * @param {Object} dbClient Instance of DynamoDB client
+ * @param  {...any} params params is destructed to id, which is the value you use to query against.
+ * The table is the table name (type String), attribute is the column you search against (type String),
+ * attributeType is the type of data stored within that column and useIndex is toggled to true if you want to use
+ * an existing index (type boolean)
+ * @returns {Object} metadata about the request, including httpStatusCode
+ */
+export const lookUp = async (dbClient, ...params) => {
+  const [id, table, attribute, attributeType, useIndex] = params;
+
+  const ExpressionAttributeValuesKey = `:${attribute}`;
+  let expressionAttributeValuesObj = {};
+  let expressionAttributeValuesNestObj = {};
+
+  expressionAttributeValuesNestObj[attributeType] = id;
+  expressionAttributeValuesObj[ExpressionAttributeValuesKey] =
+    expressionAttributeValuesNestObj;
+
+  const input = {
+    ExpressionAttributeValues: expressionAttributeValuesObj,
+    KeyConditionExpression: `${attribute} = :${attribute}`,
+    TableName: `${ENVIRONMENT}-${table}`,
+  };
+
+  if (useIndex) {
+    input.IndexName = `${attribute}-index`;
   }
+
+  const getCommand = new QueryCommand(input);
+  const response = await dbClient.send(getCommand);
+
+  if (response.$metadata.httpStatusCode != 200) {
+    console.log(`look up item input = ${JSON.stringify(input, null, 2)}`);
+  }
+
+  return response;
 };
