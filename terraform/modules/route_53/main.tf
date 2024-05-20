1431e4bd7ef6a315c860bc02c89817db12e6dd0f
Andrew Cleveland <andrew.cleveland1@nhs.net>
2024-05-20 10:29:10 +0100

    tweaked check

--- terraform/modules/route_53/main.tf
+++ terraform/modules/route_53/main.tf
@@ -40,7 +40,7 @@ resource "aws_acm_certificate_validation" "example" {
 }
 
 resource "aws_route53_record" "actual_record" {
-  count   = local.alias_name_valid ? 0 : 1
+  count   = local.alias_name_valid ? 1 : 0
   zone_id = data.aws_route53_zone.example.id
   name    = aws_acm_certificate.certificate.domain_name
   type    = "A"
