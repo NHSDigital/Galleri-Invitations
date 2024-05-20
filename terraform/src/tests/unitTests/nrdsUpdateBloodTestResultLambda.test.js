81e20a983ff75265e8edb3262f805babf743a9ea
Abdul-NHS <abdul.shaker1@nhs.net>
2024-05-20 22:02:27 +0100

    feature/GAL-1256# adding more unit tests

--- terraform/src/tests/unitTests/nrdsUpdateBloodTestResultLambda.test.js
+++ terraform/src/tests/unitTests/nrdsUpdateBloodTestResultLambda.test.js
@@ -9,6 +9,7 @@ import {
   readCsvFromS3,
   pushCsvToS3,
   lookUp,
+  transactionalWrite,
 } from "../../nrdsUpdateBloodTestResultLambda/nrdsUpdateBloodTestResultLambda.js";
 
 describe("readCsvFromS3", () => {
@@ -156,5 +157,91 @@ describe("lookUp", () => {
     );
 
     expect(result.Items).toEqual([]);
+  });
+});
+
+describe("transactionalWrite", () => {
+  const mockDynamoDbClient = mockClient(new DynamoDBClient({}));
+
+  test("Return successful response if participant exists", async () => {
+    mockDynamoDbClient.resolves({
+      $metadata: {
+        httpStatusCode: 200,
+      },
+      Items: ["I exist"],
+    });
+
+    let testObj = {
+      episode_event: "null",
+      Grail_FHIR_Result_Id: "MCED-AmendedTest-Example",
+      Meta_Last_Updated: "2000-09-11T11:22:00+00:00",
+      Identifier_Value: "example",
+      Grail_Id: "NHS1234567",
+      CSD_Result_SNOWMED_Code: "12345",
+      CSD_Result_SNOWMED_Display: "onceICaughtAFishAlive",
+      Blood_Draw_Date: "2000-09-11T11:22:00+00:00",
+      Cso_Result_Snowmed_Code_Primary: ["6"],
+      Cso_Result_Snowmed_Display_Primary: ["7"],
+      Cso_Result_Snowmed_Code_Secondary: ["8", "9", "10"],
+      Cso_Result_Snowmed_Display_Secondary: ["thenILetItGoAgain"],
+      Participant_Id: "NHS-12345",
+    };
+
+    const batchId = "IB-pck28f-datsf28f-a233-bug41-2right111f4a53";
+    const appointmentId = "12345";
+    const episodeStatus = "status";
+
+    episodeStatus;
+    const result = await transactionalWrite(
+      mockDynamoDbClient,
+      testObj.participantId,
+      batchId,
+      appointmentId,
+      testObj,
+      episodeStatus
+    );
+
+    expect(result).toEqual(true);
+  });
+
+  test("Return unsuccessful response if participant does not exist", async () => {
+    mockDynamoDbClient.resolves({
+      $metadata: {
+        httpStatusCode: 400,
+      },
+      Items: [],
+    });
+
+    let testObj = {
+      episode_event: "null",
+      Grail_FHIR_Result_Id: "MCED-AmendedTest-Example",
+      Meta_Last_Updated: "2000-09-11T11:22:00+00:00",
+      Identifier_Value: "example",
+      Grail_Id: "NHS1234567",
+      CSD_Result_SNOWMED_Code: "Erin",
+      CSD_Result_SNOWMED_Display: "Yeager",
+      Blood_Draw_Date: "2000-09-11T11:22:00+00:00",
+      Cso_Result_Snowmed_Code_Primary: ["Grisha"],
+      Cso_Result_Snowmed_Display_Primary: ["Yeager"],
+      Cso_Result_Snowmed_Code_Secondary: ["Zeke"],
+      Cso_Result_Snowmed_Display_Secondary: ["Yeager"],
+      Participant_Id: "NHS-12345",
+    };
+
+    const batchId = "IB-pck28f-datsf28f-a233-bug41-2right111f4a53";
+    const appointmentId = "12345";
+    const episodeStatus = "status";
+
+    episodeStatus;
+    const result = await transactionalWrite(
+      mockDynamoDbClient,
+      testObj.participantId,
+      batchId,
+      appointmentId,
+      testObj,
+      episodeStatus
+    );
+
+    expect(result).toEqual(false);
   });
 });
