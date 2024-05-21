740eb3b7418b222661cc1f456dcc58462c4dd5ca
fais-2 <faisal.isse1@nhs.net>
2024-05-21 19:06:20 +0100

    merge with main

---
+++ terraform/modules/cloudwatch/cloudwatch.tf
@@ -0,0 +1,9 @@
+resource "aws_cloudwatch_log_group" "lambda_log_group" {
+    name = "/aws/lambda/${var.lambda_function_name}"
+
+  retention_in_days = 14
+  tags = {
+    ApplicationRole = "${var.application_role}"
+    Name            = "${var.environment} Lambda Log Group"
+  }
+}
