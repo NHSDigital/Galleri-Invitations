969732a0a3a5022966b01c8970ac409eac493481
Abdul-NHS <abdul.shaker1@nhs.net>
2024-05-22 23:54:19 +0100

    feature/GAL-1256# fixing tests

--- terraform/src/tests/unitTests/nrdsUpdateBloodTestResultLambda.test.js
+++ terraform/src/tests/unitTests/nrdsUpdateBloodTestResultLambda.test.js
@@ -92,7 +92,7 @@ describe("pushCsvToS3", () => {
     expect(result).toHaveProperty("$metadata.httpStatusCode", 200);
   });
   test("Failed response when error occurs sending file to bucket", async () => {
-    const logSpy = jest.spyOn(global.console, "log");
+    const logSpy = jest.spyOn(global.console, "error");
     const errorMsg = new Error("Mocked error");
     const mockClient = {
       send: jest.fn().mockRejectedValue(errorMsg),
@@ -105,7 +105,7 @@ describe("pushCsvToS3", () => {
     expect(logSpy).toHaveBeenCalled();
     expect(logSpy).toHaveBeenCalledTimes(1);
     expect(logSpy).toHaveBeenCalledWith(
-      `Failed to push to galleri-ons-data/test.txt. Error Message: ${errorMsg}`
+      `Error: Failed to push to galleri-ons-data/test.txt. Error Message: ${errorMsg}`
     );
   });
 });
