5c1c1328ec2fe169fd1926f6fb75e8319969cb00
Andrew Cleveland <andrew.cleveland1@nhs.net>
2024-05-29 11:06:51 +0100

    updated waf

--- terraform/modules/api-gateway/api-gateway.tf
+++ terraform/modules/api-gateway/api-gateway.tf
@@ -69,6 +69,230 @@
 #   depends_on = [aws_api_gateway_domain_name.api_domain]
 # }
 
+
+
+
+
+# # WAF Web ACL
+# resource "aws_wafv2_web_acl" "geo_block" {
+#   name        = "${var.environment}-${var.path_part}-web-acl"
+#   description = "Web ACL to allow only UK traffic"
+#   scope       = "REGIONAL"
+#   default_action {
+#     allow {}
+#   }
+#   rule {
+#     name     = "GeoBlock"
+#     priority = 1
+#     action {
+#       block {}
+#     }
+#     statement {
+#       not_statement {
+#         statement {
+#           geo_match_statement {
+#             country_codes = ["GB"]
+#           }
+#         }
+#       }
+#     }
+#     visibility_config {
+#       cloudwatch_metrics_enabled = true
+#       metric_name                = "GeoBlock"
+#       sampled_requests_enabled   = true
+#     }
+#   }
+#   visibility_config {
+#     cloudwatch_metrics_enabled = true
+#     metric_name                = "webACL"
+#     sampled_requests_enabled   = true
+#   }
+# }
+
+# # API Gateway WAF Association
+# resource "aws_wafv2_web_acl_association" "api_gateway_waf" {
+#   resource_arn = aws_api_gateway_rest_api.galleri.arn
+#   web_acl_arn  = aws_wafv2_web_acl.geo_block.arn
+# }
+
+
+# # Set up cloudwatch logging
+# resource "aws_cloudwatch_log_group" "api_gateway_log_group" {
+#   name              = "/aws/apigateway/${var.environment}-${var.path_part}"
+#   retention_in_days = 90
+#   tags = {
+#     ApplicationRole = "${var.application_role}"
+#     Name            = "${var.environment} API Gateway Cloudwatch Logging"
+#   }
+# }
+
+# # Create API Gateway
+# resource "aws_api_gateway_rest_api" "galleri" {
+#   name        = "${var.environment}-${var.path_part}"
+#   description = "API for the galleri webapp"
+#   endpoint_configuration {
+#     types = ["REGIONAL"]
+#   }
+#   tags = {
+#     ApplicationRole = "${var.application_role}"
+#     Name            = "${var.environment} API Gateway"
+#   }
+# }
+
+# # resource "aws_api_gateway_stage" "stage" {
+# #   stage_name    = var.environment
+# #   rest_api_id   = aws_api_gateway_rest_api.galleri.id
+# #   deployment_id = aws_api_gateway_deployment.galleri.id
+
+# #   access_log_settings {
+# #     destination_arn = aws_cloudwatch_log_group.api_gateway_log_group.arn
+# #     format = jsonencode({
+# #       requestId      = "$context.requestId",
+# #       ip             = "$context.identity.sourceIp",
+# #       caller         = "$context.identity.caller",
+# #       user           = "$context.identity.user",
+# #       requestTime    = "$context.requestTime",
+# #       httpMethod     = "$context.httpMethod",
+# #       resourcePath   = "$context.resourcePath",
+# #       status         = "$context.status",
+# #       protocol       = "$context.protocol",
+# #       responseLength = "$context.responseLength"
+# #     })
+# #   }
+
+# #   xray_tracing_enabled = true
+# #   depends_on           = [aws_api_gateway_deployment.galleri]
+# # }
+
+resource "aws_api_gateway_deployment" "galleri" {
+  stage_name  = var.environment
+  rest_api_id = aws_api_gateway_rest_api.galleri.id
+  lifecycle {
+    create_before_destroy = true
+  }
+  depends_on = [
+    aws_api_gateway_method.http,
+    aws_api_gateway_method.options,
+    aws_api_gateway_integration.http,
+    aws_api_gateway_integration.options
+  ]
+}
+
+
+# // HTTP METHOD
+# resource "aws_api_gateway_resource" "gateway" {
+#   rest_api_id = aws_api_gateway_rest_api.galleri.id
+#   parent_id   = aws_api_gateway_rest_api.galleri.root_resource_id
+#   path_part   = var.path_part
+# }
+
+# resource "aws_api_gateway_method" "http" {
+#   rest_api_id   = aws_api_gateway_rest_api.galleri.id
+#   resource_id   = aws_api_gateway_resource.gateway.id
+#   http_method   = var.lambda_api_gateway_method
+#   authorization = "NONE"
+
+#   request_parameters = var.method_http_parameters
+# }
+
+# resource "aws_api_gateway_integration" "http" {
+#   rest_api_id             = aws_api_gateway_rest_api.galleri.id
+#   resource_id             = aws_api_gateway_method.http.resource_id
+#   http_method             = aws_api_gateway_method.http.http_method
+#   integration_http_method = var.integration_http_method
+#   type                    = "AWS_PROXY"
+#   uri                     = var.lambda_invoke_arn
+
+#   depends_on = [aws_api_gateway_method.http]
+# }
+
+# resource "aws_api_gateway_method_response" "http" {
+#   rest_api_id = aws_api_gateway_rest_api.galleri.id
+#   resource_id = aws_api_gateway_method.http.resource_id
+#   http_method = aws_api_gateway_method.http.http_method
+#   status_code = 200
+
+#   response_models = {
+#     "application/json" = "Empty"
+#   }
+
+#   response_parameters = var.method_response_http_parameters
+#   depends_on          = [aws_api_gateway_method.http]
+# }
+
+# resource "aws_api_gateway_integration_response" "http" {
+#   rest_api_id = aws_api_gateway_rest_api.galleri.id
+#   resource_id = aws_api_gateway_resource.gateway.id
+#   http_method = aws_api_gateway_method.http.http_method
+#   status_code = aws_api_gateway_method_response.http.status_code
+
+#   response_parameters = var.integration_response_http_parameters
+#   depends_on          = [aws_api_gateway_integration.http]
+# }
+
+
+# // OPTIONS METHOD
+# resource "aws_api_gateway_method" "options" {
+#   rest_api_id   = aws_api_gateway_rest_api.galleri.id
+#   resource_id   = aws_api_gateway_resource.gateway.id
+#   http_method   = "OPTIONS"
+#   authorization = "NONE"
+# }
+
+# resource "aws_api_gateway_integration" "options" {
+#   rest_api_id = aws_api_gateway_rest_api.galleri.id
+#   resource_id = aws_api_gateway_method.options.resource_id
+#   http_method = aws_api_gateway_method.options.http_method
+
+#   type = "MOCK"
+#   request_templates = { # Not documented
+#     "application/json" = "{statusCode: 200}"
+#   }
+
+#   depends_on = [aws_api_gateway_method.options]
+# }
+
+# resource "aws_api_gateway_method_response" "options" {
+#   rest_api_id = aws_api_gateway_rest_api.galleri.id
+#   resource_id = aws_api_gateway_resource.gateway.id
+#   http_method = aws_api_gateway_method.options.http_method
+#   status_code = 200
+
+#   response_models = {
+#     "application/json" = "Empty"
+#   }
+
+#   response_parameters = var.method_response_options_parameters
+
+#   depends_on = [aws_api_gateway_method.options]
+# }
+
+# resource "aws_api_gateway_integration_response" "options" {
+#   rest_api_id         = aws_api_gateway_rest_api.galleri.id
+#   resource_id         = aws_api_gateway_resource.gateway.id
+#   http_method         = aws_api_gateway_method.options.http_method
+#   status_code         = aws_api_gateway_method_response.options.status_code
+#   response_parameters = var.integration_response_options_parameters
+
+#   depends_on = [aws_api_gateway_integration.options]
+# }
+
+# # Lambda Permissions
+
+# resource "aws_lambda_permission" "lambda_permission" {
+#   statement_id  = var.statement_id
+#   action        = var.action
+#   function_name = var.lambda_function_name
+#   principal     = var.principal
+
+#   # The /*/* portion grants access from any method on any resource
+#   # within the API Gateway "REST API".
+#   source_arn = "${aws_api_gateway_rest_api.galleri.execution_arn}${var.method}"
+# }
+
+
+
+
 # WAF Web ACL
 resource "aws_wafv2_web_acl" "geo_block" {
   name        = "${var.environment}-${var.path_part}-web-acl"
@@ -105,12 +329,37 @@ resource "aws_wafv2_web_acl" "geo_block" {
   }
 }
 
-# API Gateway WAF Association
-resource "aws_wafv2_web_acl_association" "api_gateway_waf" {
-  resource_arn = aws_api_gateway_rest_api.galleri.arn
-  web_acl_arn  = aws_wafv2_web_acl.geo_block.arn
+# Create API Gateway Stage
+resource "aws_api_gateway_stage" "stage" {
+  stage_name    = var.environment
+  rest_api_id   = aws_api_gateway_rest_api.galleri.id
+  deployment_id = aws_api_gateway_deployment.galleri.id
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.api_gateway_log_group.arn
+    format = jsonencode({
+      requestId      = "$context.requestId",
+      ip             = "$context.identity.sourceIp",
+      caller         = "$context.identity.caller",
+      user           = "$context.identity.user",
+      requestTime    = "$context.requestTime",
+      httpMethod     = "$context.httpMethod",
+      resourcePath   = "$context.resourcePath",
+      status         = "$context.status",
+      protocol       = "$context.protocol",
+      responseLength = "$context.responseLength"
+    })
+  }
+
+  xray_tracing_enabled = true
+  depends_on           = [aws_api_gateway_deployment.galleri]
 }
 
+# API Gateway WAF Association
+resource "aws_wafv2_web_acl_association" "api_gateway_waf" {
+  resource_arn = aws_api_gateway_stage.stage.arn
+  web_acl_arn  = aws_wafv2_web_acl.geo_block.arn
+}
 
 # Set up cloudwatch logging
 resource "aws_cloudwatch_log_group" "api_gateway_log_group" {
@@ -135,47 +384,7 @@ resource "aws_api_gateway_rest_api" "galleri" {
   }
 }
 
-# resource "aws_api_gateway_stage" "stage" {
-#   stage_name    = var.environment
-#   rest_api_id   = aws_api_gateway_rest_api.galleri.id
-#   deployment_id = aws_api_gateway_deployment.galleri.id
-
-#   access_log_settings {
-#     destination_arn = aws_cloudwatch_log_group.api_gateway_log_group.arn
-#     format = jsonencode({
-#       requestId      = "$context.requestId",
-#       ip             = "$context.identity.sourceIp",
-#       caller         = "$context.identity.caller",
-#       user           = "$context.identity.user",
-#       requestTime    = "$context.requestTime",
-#       httpMethod     = "$context.httpMethod",
-#       resourcePath   = "$context.resourcePath",
-#       status         = "$context.status",
-#       protocol       = "$context.protocol",
-#       responseLength = "$context.responseLength"
-#     })
-#   }
-
-#   xray_tracing_enabled = true
-#   depends_on           = [aws_api_gateway_deployment.galleri]
-# }
-
-resource "aws_api_gateway_deployment" "galleri" {
-  stage_name  = var.environment
-  rest_api_id = aws_api_gateway_rest_api.galleri.id
-  lifecycle {
-    create_before_destroy = true
-  }
-  depends_on = [
-    aws_api_gateway_method.http,
-    aws_api_gateway_method.options,
-    aws_api_gateway_integration.http,
-    aws_api_gateway_integration.options
-  ]
-}
-
-
-// HTTP METHOD
+# HTTP METHOD
 resource "aws_api_gateway_resource" "gateway" {
   rest_api_id = aws_api_gateway_rest_api.galleri.id
   parent_id   = aws_api_gateway_rest_api.galleri.root_resource_id
@@ -226,8 +435,7 @@ resource "aws_api_gateway_integration_response" "http" {
   depends_on          = [aws_api_gateway_integration.http]
 }
 
-
-// OPTIONS METHOD
+# OPTIONS METHOD
 resource "aws_api_gateway_method" "options" {
   rest_api_id   = aws_api_gateway_rest_api.galleri.id
   resource_id   = aws_api_gateway_resource.gateway.id
@@ -274,14 +482,10 @@ resource "aws_api_gateway_integration_response" "options" {
 }
 
 # Lambda Permissions
-
 resource "aws_lambda_permission" "lambda_permission" {
   statement_id  = var.statement_id
   action        = var.action
   function_name = var.lambda_function_name
   principal     = var.principal
-
-  # The /*/* portion grants access from any method on any resource
-  # within the API Gateway "REST API".
-  source_arn = "${aws_api_gateway_rest_api.galleri.execution_arn}${var.method}"
+  source_arn    = "${aws_api_gateway_rest_api.galleri.execution_arn}${var.method}"
 }
