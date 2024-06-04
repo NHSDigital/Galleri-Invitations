cf295ea92b0c094ddfa15e6dc6f84d8a1a64293f
Serena-NHS <serena.sharma5@nhs.net>
2024-06-04 17:45:15 +0100

    Formatting

--- terraform/src/onwardReferralListLambda/onwardReferralListLambda.js
+++ terraform/src/onwardReferralListLambda/onwardReferralListLambda.js
@@ -109,97 +109,6 @@ export const lookupParticipantsInfo = async (participantList, client, lastEvalua
       console.log("------participantIds-------", JSON.stringify(participantIds));
       console.log("------participantIds[0]-------", JSON.stringify(participantIds[0]));
 
-      // //var titleValues = ["The Big New Movie 2012", "The Big New Movie"];
-      // var idsObject = {};
-      // var index = 0;
-      // participantIds.forEach(function (id) {
-      //   index++;
-      //   var titleKey = ":participantId" + index;
-      //   idsObject[titleKey.toString()] = id;
-      // });
-
-      // console.log("------idsObject-------", JSON.stringify(idsObject));
-      // console.log("------Object.keys(idsObject).toString()-------", Object.keys(idsObject).toString());
-
-      // const input = {
-      //   ExpressionAttributeNames: {
-      //     "#PI": "Participant_Id",
-      //     "#BDD": "Blood_Draw_Date",
-      //     "#RC": "Result_Creation",
-      //     "#PN": "Participant_Name",
-      //   },
-      //   ExpressionAttributeValues: idsObject,
-      //   FilterExpression: "#PI IN (" + Object.keys(idsObject).toString() + ")",
-      //   ProjectionExpression: "#PI, #BDD, #RC, #PN",
-      //   TableName: `${ENVIRONMENT}-GalleriBloodTestResult`,
-      // };
-
-      // if (Object.keys(lastEvaluatedItem).length != 0) {
-      //   input.ExclusiveStartKey = lastEvaluatedItem;
-      // }
-
-      // const command = new ScanCommand(input);
-      // const response = await client.send(command);
-
-
-
-      console.log("------1111111-------");
-
-      var abc = "NHS-DL50-ER34";
-      const input1 = {
-        ExpressionAttributeNames: {
-          "#PI": "Participant_Id",
-          "#BDD": "Blood_Draw_Date",
-          "#RC": "Result_Creation",
-          "#PN": "Participant_Name",
-        },
-        ExpressionAttributeValues: {
-          ":a": {
-            S: "NHS-DL50-ER34",
-          },
-        },
-        FilterExpression: "Participant_Id = :a",
-        ProjectionExpression: "#PI, #BDD, #RC, #PN",
-        TableName: `${ENVIRONMENT}-GalleriBloodTestResult`,
-      };
-
-      const command1 = new ScanCommand(input1);
-      const response1 = await client.send(command1);
-      console.log("------response1-------", response1.Items);
-
-
-
-      console.log("------1111111222-------");
-
-      var abc = "NHS-DL50-ER34";
-      const input12 = {
-        ExpressionAttributeNames: {
-          "#PI": "Participant_Id",
-          "#BDD": "Blood_Draw_Date",
-          "#RC": "Result_Creation",
-          "#PN": "Participant_Name",
-        },
-        ExpressionAttributeValues: {
-          ":a": {
-            S: "NHS-DL50-ER34",
-          },
-        },
-        FilterExpression: "Participant_Id IN (:a)",
-        ProjectionExpression: "#PI, #BDD, #RC, #PN",
-        TableName: `${ENVIRONMENT}-GalleriBloodTestResult`,
-      };
-
-      const command12 = new ScanCommand(input12);
-      const response12 = await client.send(command12);
-      console.log("------response122-------", response12.Items);
-
-
-
-
-
-
-      console.log("------222222222-------");
-
       var idsObject = {};
       var index = 0;
       participantIds.forEach(function (id) {
@@ -211,7 +120,7 @@ export const lookupParticipantsInfo = async (participantList, client, lastEvalua
       console.log("------idsObject-------", JSON.stringify(idsObject));
       console.log("------Object.keys(idsObject).toString()-------", Object.keys(idsObject).toString());
 
-      const input2 = {
+      const input = {
         ExpressionAttributeNames: {
           "#PI": "Participant_Id",
           "#BDD": "Blood_Draw_Date",
@@ -224,13 +133,9 @@ export const lookupParticipantsInfo = async (participantList, client, lastEvalua
         TableName: `${ENVIRONMENT}-GalleriBloodTestResult`,
       };
 
-      const command2 = new ScanCommand(input2);
-      const response2 = await client.send(command2);
-      console.log("------response2-------", response2.Items);
-
-
-
-
+      const command = new ScanCommand(input);
+      const response = await client.send(command);
+      console.log("------response-------", response.Items);
 
       if (response.LastEvaluatedKey) {
         console.log("------1-------", response.LastEvaluatedKey);
