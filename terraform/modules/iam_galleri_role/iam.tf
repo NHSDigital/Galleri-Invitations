9646cdb67dc9b7bdaf11f09b2ea0e140f22e78fb
Aymene Nibouche <aymene.nibouche1@nhs.net>
2024-06-06 14:12:32 +0100

    Exceeded policy limit

--- terraform/modules/iam_galleri_role/iam.tf
+++ terraform/modules/iam_galleri_role/iam.tf
@@ -331,7 +331,9 @@ resource "aws_iam_policy" "iam_policy_for_get_lsoa_in_range_lambda" {
             "dynamodb:*"
           ],
           "Resource" : [
-            "arn:aws:dynamodb:eu-west-2:${var.account_id}:table/${var.environment}-UniqueLsoa"
+            "arn:aws:dynamodb:eu-west-2:${var.account_id}:table/${var.environment}-UniqueLsoa",
+            "arn:aws:dynamodb:eu-west-2:${var.account_id}:table/${var.environment}-CancerSignalOrigin",
+            "arn:aws:dynamodb:eu-west-2:${var.account_id}:table/${var.environment}-CancerSignalOrigin/*/*"
           ]
         },
         {
@@ -667,9 +669,7 @@ resource "aws_iam_policy" "iam_policy_for_participants_in_lsoa_lambda" {
             "arn:aws:dynamodb:eu-west-2:${var.account_id}:table/${var.environment}-NotifySendMessageStatus",
             "arn:aws:dynamodb:eu-west-2:${var.account_id}:table/${var.environment}-APIGatewayLockdownSession",
             "arn:aws:dynamodb:eu-west-2:${var.account_id}:table/${var.environment}-GalleriBloodTestResult",
-            "arn:aws:dynamodb:eu-west-2:${var.account_id}:table/${var.environment}-GalleriBloodTestResult/*/*",
-            "arn:aws:dynamodb:eu-west-2:${var.account_id}:table/${var.environment}-CancerSignalOrigin",
-            "arn:aws:dynamodb:eu-west-2:${var.account_id}:table/${var.environment}-CancerSignalOrigin/*/*"
+            "arn:aws:dynamodb:eu-west-2:${var.account_id}:table/${var.environment}-GalleriBloodTestResult/*/*"
           ]
         },
         {
