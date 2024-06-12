56b761e16bd01c481b7cf942eec934e6395f28f7
Abdul-NHS <abdul.shaker1@nhs.net>
2024-06-12 14:51:57 +0100

    AS: added jsdoc comments to nrds mesh lambda

--- terraform/src/nrdsMeshMailboxLambda/helper.js
+++ terraform/src/nrdsMeshMailboxLambda/helper.js
@@ -1,8 +1,26 @@
-import { SecretsManagerClient, GetSecretValueCommand } from "@aws-sdk/client-secrets-manager";
+import {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} from "@aws-sdk/client-secrets-manager";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
-import { handShake, getMessageCount, markAsRead, readMessage } from "nhs-mesh-client";
+import {
+  handShake,
+  getMessageCount,
+  markAsRead,
+  readMessage,
+} from "nhs-mesh-client";
 
-//Push string from MESH to S3
+/**
+ * Pushes a CSV file to S3.
+ *
+ * @function pushCsvToS3
+ * @async
+ * @param {string} bucketName - The name of the S3 bucket.
+ * @param {string} key - The key of the object to be saved in the S3 bucket.
+ * @param {string} body - The contents of the object to be saved in the S3 bucket.
+ * @param {S3Client} client - An instance of the S3 client.
+ * @returns {Promise<Object>} Resolves to the response from the S3 client.
+ */
 export const pushCsvToS3 = async (bucketName, key, body, client) => {
   try {
     const response = await client.send(
@@ -19,13 +37,21 @@ export const pushCsvToS3 = async (bucketName, key, body, client) => {
     throw err;
   }
 };
-
-//Return 'Secret value' from secrets manager by passing in 'Secret name'
+/**
+ * Retrieves 'Secret value' from secrets manager by passing in 'Secret name' from AWS Secrets Manager
+ *
+ * @function getSecret
+ * @async
+ * @param {string} secretName - Name of the secret to retrieve
+ * @param {SecretsManagerClient} client - Secrets Manager client
+ * @returns {string} Secret value
+ * @throws {Error} If there is an error retrieving the secret
+ */
 export const getSecret = async (secretName, client) => {
   try {
     const response = await client.send(
       new GetSecretValueCommand({
-        SecretId: secretName
+        SecretId: secretName,
       })
     );
     console.log(`Retrieved value successfully ${secretName}`);
@@ -34,14 +60,16 @@ export const getSecret = async (secretName, client) => {
     console.log(`Failed: ${error}`);
     throw error;
   }
-}
+};
 
-/*
+/**
  * Establish connection with MESH
- * @params handshake to be replaced with handShake fn
+ *
+ * @param {Object} CONFIG - configuration obj includes mailbox id, certs etc
+ * @param {string} handshake - handshake to be replaced with handShake fn
+ * @returns {Promise<string>}
  */
 export const getHealthStatusCode = async (CONFIG, handshake) => {
-
   try {
     const healthCheck = await handshake({
       url: CONFIG.url,
@@ -55,11 +83,14 @@ export const getHealthStatusCode = async (CONFIG, handshake) => {
   } catch (error) {
     console.error(`Error occurred: ${error}`);
   }
-}
+};
 
-/*
+/**
  * Return an array of message IDs from MESH
- * @params msgCount to be replaced with getMessageCount fn
+ *
+ * @param {Object} CONFIG - configuration obj includes mailbox id, certs etc
+ * @param {string} msgCount - msgCount to be replaced with getMessageCount fn
+ * @returns
  */
 export const getMessageArray = async (CONFIG, msgCount) => {
   try {
@@ -69,8 +100,8 @@ export const getMessageArray = async (CONFIG, msgCount) => {
       mailboxPassword: CONFIG.receiverMailboxPassword,
       sharedKey: CONFIG.sharedKey,
       agent: CONFIG.receiverAgent,
-    })
-    let messageList = messageCount.data.messages
+    });
+    let messageList = messageCount.data.messages;
     let inboxCount = messageCount.data.approx_inbox_count;
     if (!inboxCount) {
       inboxCount = 0;
@@ -80,12 +111,15 @@ export const getMessageArray = async (CONFIG, msgCount) => {
   } catch (error) {
     console.error("Error occurred:", error);
   }
-}
+};
 
-/*
- * Marks messaged as read based on the message ID passed in,
- * subsequently removing message from MESH mailbox
- * @params marked to be replaced with markAsRead fn
+/**
+ * Marks messaged as read based on the message ID passed in, subsequently removing message from MESH mailbox
+ *
+ * @param {*} CONFIG - configuration obj includes mailbox id, certs etc
+ * @param {*} marked - marked to be replaced with markAsRead fn
+ * @param {*} msgID - message you want to querying
+ * @returns
  */
 export const markRead = async (CONFIG, marked, msgID) => {
   try {
@@ -101,11 +135,14 @@ export const markRead = async (CONFIG, marked, msgID) => {
   } catch (error) {
     console.error(`Error occurred: ${error}`);
   }
-}
+};
 
-/*
- * Reads message data from MESH based on message ID
- * @params readingMsg to be replaced with readMessage fn
+/**
+ *
+ * @param {*} CONFIG - configuration obj includes mailbox id, certs etc
+ * @param {*} readingMsg - readingMsg to be replaced with readMessage fn
+ * @param {*} msgID - message you want to querying
+ * @returns
  */
 export const readMsg = async (CONFIG, readingMsg, msgID) => {
   try {
@@ -121,4 +158,4 @@ export const readMsg = async (CONFIG, readingMsg, msgID) => {
   } catch (error) {
     console.error("Error occurred:", error);
   }
-}
+};
